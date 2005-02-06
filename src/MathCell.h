41fcd377fe2f8647eb4c1ca8e1ab714e8ae1c67f
Andrej Vodopivec <andrejv@users.sourceforge.net>
2005-02-06 12:31:51 +0000

    Fix SCALE_PX
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@38 cef4cce0-9ccc-409e-9180-6d2a30c8385f

--- src/MathCell.h
+++ src/MathCell.h
@@ -24,7 +24,7 @@
 #define MAX(a,b) ((a)>(b) ? (a) : (b))
 #define MIN(a,b) ((a)>(b) ? (b) : (a))
 #define ABS(a) ((a)>=0 ? (a) : -(a))
-#define SCALE_PX(px, scale) ((int)(((double)px)*scale + 0.5))
+#define SCALE_PX(px, scale) ((int)(((double)(px))*(scale) + 0.5))
 
 #include <wx/wx.h>
 #include "CellParser.h"
