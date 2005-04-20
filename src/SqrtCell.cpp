a051dec4a562f34c4f8de22ff5b54d77a0306537
Andrej Vodopivec <andrejv@users.sourceforge.net>
2005-04-20 08:46:22 +0000

    Small changes to display
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@165 cef4cce0-9ccc-409e-9180-6d2a30c8385f

--- src/SqrtCell.cpp
+++ src/SqrtCell.cpp
@@ -65,7 +65,8 @@ void SqrtCell::RecalculateWidths(CellParser& parser, int fontsize, bool all)
 {
   double scale = parser.GetScale();
   m_innerCell->RecalculateWidths(parser, fontsize, true);
-  m_width = m_innerCell->GetFullWidth(scale) + SCALE_PX(13, scale);
+  m_width = m_innerCell->GetFullWidth(scale) + SCALE_PX(10, scale) +
+            3*SCALE_PX(1, scale);
   MathCell::RecalculateWidths(parser, fontsize, all);
 }
 
@@ -85,7 +86,7 @@ void SqrtCell::Draw(CellParser& parser, wxPoint point, int fontsize, bool all)
     double scale = parser.GetScale();
     
     wxPoint in(point);
-    in.x += SCALE_PX(11, scale);
+    in.x += SCALE_PX(10, scale) + SCALE_PX(1, scale);
     m_innerCell->Draw(parser, in, fontsize, true);
   
     SetPen(parser);
