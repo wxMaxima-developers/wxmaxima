0dae3eefe7fd14bb6c0e479f0b57410158affc6b
Andrej Vodopivec <andrejv@users.sourceforge.net>
2006-03-26 01:24:55 +0000

    Adding private tip provider
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@336 cef4cce0-9ccc-409e-9180-6d2a30c8385f

---
+++ src/MyTipProvider.h
@@ -0,0 +1,38 @@
+/*
+ *  Copyright (C) 2006 Andrej Vodopivec <andrejv@users.sourceforge.net>
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ */
+
+#ifndef _MYTIPPROVIDER_H_
+#define _MYTIPPROVIDER_H_
+
+#include <wx/wx.h>
+#include <wx/tipdlg.h>
+
+class MyTipProvider : public wxTipProvider {
+  public:
+    MyTipProvider(const wxString& filename, int n);
+    ~MyTipProvider();
+    wxString GetTip();
+    int GetCurrentTip() { return m_current; }
+  private:
+    int m_current;
+    wxTextFile m_file;
+};
+
+#endif
