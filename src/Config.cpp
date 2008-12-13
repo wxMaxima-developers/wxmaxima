e89586f65041ffcd48511bbcd1314e7f6434d588
Andrej Vodopivec <andrejv@users.sourceforge.net>
2008-12-13 09:08:31 +0000

    Changed setting label
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@799 cef4cce0-9ccc-409e-9180-6d2a30c8385f

--- src/Config.cpp
+++ src/Config.cpp
@@ -112,7 +112,7 @@ Config::Config(wxWindow* parent, int id, const wxString& title,
   m_showLong = new wxCheckBox(notebook_1_pane_1, -1, _("Show long expressions"));
   m_showHeader = new wxCheckBox(notebook_1_pane_1, -1, _("Show Maxima header"));
   m_unixCopy = new wxCheckBox(notebook_1_pane_1, -1, _("Copy to clipboard on select"));
-  m_changeAsterisk = new wxCheckBox(notebook_1_pane_1, -1, _("Display \xB7 insead of *"));
+  m_changeAsterisk = new wxCheckBox(notebook_1_pane_1, -1, _("Use centered dot character for multiplication"));
   label_8 = new wxStaticText(notebook_1_pane_2, -1, _("Default font:"));
   m_getFont = new wxButton(notebook_1_pane_2, font_family, _("Choose font"), wxDefaultPosition, wxSize(250, -1));
   m_greekFontOk = new wxCheckBox(notebook_1_pane_2, checkbox_greek, _("Use greek font:"));
