6bd7dd82d085b0de14aa61a5c8bcef8164121099
Andrej Vodopivec <andrejv@users.sourceforge.net>
2009-01-29 22:22:16 +0000

    Console -> document
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@855 cef4cce0-9ccc-409e-9180-6d2a30c8385f

--- src/Config.cpp
+++ src/Config.cpp
@@ -180,19 +180,19 @@ void Config::set_properties()
                                        " (e.g. -l clisp)."));
   m_saveSize->SetToolTip(_("Save wxMaxima window size/position between sessions."));
   m_matchParens->SetToolTip(_("Write matching parenthesis in text controls."));
-  m_showLong->SetToolTip(_("Show long expressions in wxMaxima console."));
+  m_showLong->SetToolTip(_("Show long expressions in wxMaxima document."));
   m_language->SetToolTip(_("Language used for wxMaxima GUI."));
   m_showHeader->SetToolTip(_("Show initial header with Maxima system information."));
   m_fixedFontInTC->SetToolTip(_("Set fixed font in text controls."));
   m_fixedFontInTC->SetToolTip(_("Set fixed font in text controls."));
-  m_getFont->SetToolTip(_("Font used for display in console."));
+  m_getFont->SetToolTip(_("Font used for display in document."));
   m_greekFontOk->SetToolTip(_("Use greek font to display greek characters."));
-  m_getGreekFont->SetToolTip(_("Font used for displaying greek characters in console."));
+  m_getGreekFont->SetToolTip(_("Font used for displaying greek characters in document."));
   m_greekFontAdj->SetToolTip(_("Adjustment for the size of greek font."));
-  m_unixCopy->SetToolTip(_("Copy selection to clipboard when selection is made in console."));
+  m_unixCopy->SetToolTip(_("Copy selection to clipboard when selection is made in document."));
   m_changeAsterisk->SetToolTip(_("Use centered dot character for multiplication"));
 #if !defined __WXMSW__ && (wxUSE_UNICODE && WXM_UNICODE_GLYPHS)
-  m_getUnicodeFont->SetToolTip(_("Font used for displaying unicode glyphs in console."));
+  m_getUnicodeFont->SetToolTip(_("Font used for displaying unicode glyphs in document."));
 #endif
   m_defaultPort->SetToolTip(_("The default port used for communication between Maxima and wxMaxima."));
 
