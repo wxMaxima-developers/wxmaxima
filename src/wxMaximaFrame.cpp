7f1ffe3869ea96dc2f8d70770c027cec34f995ba
zigalenarcic <zigalenarcic@users.sourceforge.net>
2009-02-14 13:05:08 +0000

    Windows accelerator (ctrl-enter), rename event id to menu_evaluate
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@886 cef4cce0-9ccc-409e-9180-6d2a30c8385f

--- src/wxMaximaFrame.cpp
+++ src/wxMaximaFrame.cpp
@@ -91,7 +91,7 @@ wxMaximaFrame::wxMaximaFrame(wxWindow* parent, int id, const wxString& title,
 
 #if defined __WXMSW__
   wxAcceleratorEntry entries[0];
-  entries[0].Set(wxACCEL_CTRL,  WXK_RETURN, menu_reeval_input);
+  entries[0].Set(wxACCEL_CTRL,  WXK_RETURN, menu_evaluate);
   wxAcceleratorTable accel(1, entries);
   SetAcceleratorTable(accel);
 #endif
