789a5ace2fe61a72240092de2a86aa503fade13b
Andrej Vodopivec <andrejv@users.sourceforge.net>
2010-06-03 17:44:38 +0000

    ToString: correction for m_altCopyText.
    git-svn-id: https://wxmaxima.svn.sourceforge.net/svnroot/wxmaxima/trunk@1380 cef4cce0-9ccc-409e-9180-6d2a30c8385f

--- src/FunCell.cpp
+++ src/FunCell.cpp
@@ -125,7 +125,7 @@ wxString FunCell::ToString(bool all)
   if (m_isBroken)
     return wxEmptyString;
   if (m_altCopyText)
-    return m_altCopyText;
+    return m_altCopyText + MathCell::ToString(all);
   wxString s = m_nameCell->ToString(true) + m_argCell->ToString(true);
   s += MathCell::ToString(all);
   return s;
