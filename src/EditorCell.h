2bde754d9cd7955806339fc64effa77ef68cd514
Andrej Vodopivec <andrej.vodopivec@gmail.com>
2011-05-04 09:49:14 +0200

    changed my email

--- src/EditorCell.h
+++ src/EditorCell.h
@@ -1,5 +1,5 @@
 ///
-///  Copyright (C) 2006-2011 Andrej Vodopivec <andrejv@users.sourceforge.net>
+///  Copyright (C) 2006-2011 Andrej Vodopivec <andrej.vodopivec@gmail.com>
 ///
 ///  This program is free software; you can redistribute it and/or modify
 ///  it under the terms of the GNU General Public License as published by
