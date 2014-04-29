73a013ffaa14c49361618b2eba83057a777ef689
Gunter Königsmann <gunter@peterpall.de>
2014-04-29 07:48:19 +0200

    Revert "Will move it to a branch."
    This reverts commit b2a302116eb0a378304f833da6a8930700f0dbc4.

--- data/wxmathml.lisp
+++ data/wxmathml.lisp
@@ -1236,15 +1236,6 @@
 (defmvar $wxplot_size '((mlist simp) 500 300))
 (defmvar $wxplot_old_gnuplot nil)
 (defmvar $wxplot_pngcairo nil)
-(cond
-  ((like (simplify
-             (MFUNCTION-CALL $SYSTEM
-                 '"(gnuplot -e 'set term pngcairo')"))
-         0.)
-   (IF (NOT (BOUNDP '$WXPLOT_PNGCAIRO))
-       (ADD2LNC '$WXPLOT_PNGCAIRO $VALUES))
-   (SETQ $WXPLOT_PNGCAIRO T)))
- 
 
 (defvar *image-counter* 0)
 
