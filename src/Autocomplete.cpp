ace19a64de5a668929f203dca5c9a684b048f111
Gunter Königsmann <gunter@peterpall.de>
2015-04-08 06:50:50 +0200

    Updated the copyright notice for the autocompletion.

--- src/Autocomplete.cpp
+++ src/Autocomplete.cpp
@@ -1,6 +1,7 @@
 // -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode: nil -*-
 //
 //  Copyright (C) 2009-2015 Andrej Vodopivec <andrej.vodopivec@gmail.com>
+//  Copyright (C) 2015 Gunter Königsmann     <wxMaxima@physikbuch.de>
 //
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
