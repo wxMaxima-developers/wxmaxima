437a9e88a539735f00db74209567d6968c1e54cd
Gunter Königsmann <gunter@peterpall.de>
2015-09-08 20:44:54 +0200

    More efficient handling for big and jpeg-encoded images.

---
+++ src/Image.cpp
@@ -0,0 +1,211 @@
+#include "Image.h"
+#include <wx/mstream.h>
+#include <wx/wfstream.h>
+
+wxMemoryBuffer Image::ReadCompressedImage(wxInputStream *data)
+{
+  wxMemoryBuffer retval;
+
+  char *buf = new char[8192];
+
+  while(data->CanRead())
+    {
+      data->Read(buf,8192);
+      size_t siz;
+      retval.AppendData(buf,siz=data->LastRead());
+    }
+  
+  delete [] buf;
+  return retval;
+}
+
+wxBitmap Image::GetUnscaledBitmap()
+{
+  wxMemoryInputStream istream(m_compressedImage.GetData(),m_compressedImage.GetDataLen());
+  wxImage img(istream, wxBITMAP_TYPE_ANY);
+  wxBitmap bmp;
+  if(img.Ok())
+    bmp = wxBitmap(img);
+  return bmp;
+}
+
+Image::Image()
+{
+  m_viewportWidth  = 640;
+  m_viewportHeight = 480;
+  m_scale          = 1;  
+  m_scaledBitmap.Create (0,0);
+}
+
+Image::Image(wxBitmap bitmap)
+{
+  m_viewportWidth  = 640;
+  m_viewportHeight = 480;
+  m_scale          = 1;  
+  LoadImage(bitmap);
+}
+
+// constructor which loads an image
+Image::Image(wxString image,bool remove, wxFileSystem *filesystem)
+{
+  m_fileSystem = filesystem; // != NULL when loading from wxmx
+  m_viewportWidth  = 640;
+  m_viewportHeight = 480;
+  m_scale          = 1;
+  m_scaledBitmap.Create (0,0);
+  LoadImage(image, remove);
+}
+
+wxSize Image::ToImageFile(wxString filename)
+{
+  wxFile file(filename,wxFile::write);
+  if(!file.IsOpened())
+    return wxSize(-1,-1);
+    
+  file.Write(m_compressedImage.GetData(), m_compressedImage.GetDataLen());
+  if(file.Close())
+    return wxSize(m_originalWidth,m_originalHeight);
+  else
+    return wxSize(-1,-1);
+}
+
+wxBitmap Image::GetBitmap()
+{
+  ViewportSize(m_viewportWidth,m_viewportHeight,m_scale);
+
+  // Let's see if we have cached the scaled bitmap with the right size
+  if(m_scaledBitmap.GetWidth() == m_width)
+    return m_scaledBitmap;
+
+
+  // Seems like we need to create a new scaled bitmap.
+  if(m_scaledBitmap.GetWidth()!=m_width)
+    {
+      wxMemoryInputStream istream(m_compressedImage.GetData(),m_compressedImage.GetDataLen());
+      wxImage img(istream, wxBITMAP_TYPE_ANY);
+      if(img.Ok())
+	  m_scaledBitmap = wxBitmap(img);
+      else
+	{
+	  // Create a "image not loaded" bitmap.
+	  m_scaledBitmap.Create(400, 250);
+	  
+	  wxString error(_("Error"));
+	  
+	  wxMemoryDC dc;
+	  dc.SelectObject(m_scaledBitmap);
+	  
+	  int width = 0, height = 0;
+	  dc.GetTextExtent(error, &width, &height);
+	  
+	  dc.DrawRectangle(0, 0, 400, 250);
+	  dc.DrawLine(0, 0,   400, 250);
+	  dc.DrawLine(0, 250, 400, 0);
+	  dc.DrawText(error, 200 - width/2, 125 - height/2);
+	  
+	  dc.GetTextExtent(error, &width, &height);
+	  dc.DrawText(error, 200 - width/2, 150 - height/2);
+	}
+    }
+  
+  // Make sure we stay within sane defaults
+  if(m_width<1)m_width = 1;
+  if(m_height<1)m_height = 1;
+  
+  m_scaledBitmap = m_scaledBitmap.ConvertToImage().Rescale(m_width, m_height,wxIMAGE_QUALITY_BICUBIC);
+  return m_scaledBitmap;
+}
+
+void Image::LoadImage(wxBitmap bitmap)
+{
+  // Convert the bitmap to a png image we can use as m_compressedImage
+  wxImage image = bitmap.ConvertToImage();
+  wxMemoryOutputStream stream;
+  image.SaveFile(stream,wxBITMAP_TYPE_PNG);
+  m_compressedImage.AppendData(stream.GetOutputStreamBuffer()->GetBufferStart(),
+			       stream.GetOutputStreamBuffer()->GetBufferSize());
+
+  // Set the info about the image.
+  m_extension = wxT("png");
+  m_originalWidth  = image.GetWidth();
+  m_originalHeight = image.GetHeight();
+}
+
+void Image::LoadImage(wxString image, bool remove)
+{
+  m_compressedImage.Clear();
+  m_scaledBitmap.Create (0,0);
+  
+  if (m_fileSystem) {
+    wxFSFile *fsfile = m_fileSystem->OpenFile(image);
+    if (fsfile) { // open successful
+
+      wxInputStream *istream = fsfile->GetStream();
+
+      m_compressedImage = ReadCompressedImage(istream);
+    }
+    m_fileSystem = NULL;
+  }
+  else {
+    wxFile file(image);
+    wxFileInputStream strm(file);
+    m_compressedImage = ReadCompressedImage(&strm);
+  }
+
+  wxImage Image;
+  if(m_compressedImage.GetDataLen()>0)
+    {
+      wxMemoryInputStream istream(m_compressedImage.GetData(),m_compressedImage.GetDataLen());
+      Image.LoadFile(istream);
+    }
+  
+  m_extension = wxFileName(image).GetExt();
+
+  if(Image.Ok())
+    {
+      m_originalWidth  = Image.GetWidth();
+      m_originalHeight = Image.GetHeight();
+    }
+  else
+    {
+      // Leave space for an image showing an error message
+      m_originalWidth  = 400;
+      m_originalHeight = 250;
+    }
+  ViewportSize(m_viewportWidth,m_viewportHeight,m_scale);
+
+}
+
+void Image::ViewportSize(size_t viewPortWidth,size_t viewPortHeight,double scale)
+{
+  int width  = m_originalWidth;
+  int height = m_originalHeight;
+  m_viewportWidth = viewPortWidth;
+  m_viewportHeight= viewPortHeight;
+  m_scale = scale;
+
+  // Ensure a minimum scaling for images.
+  if(scale < 1.0) scale = 1.0;
+  
+  if((width == 0) || (height == 0))
+    {
+      m_width = 400;
+      m_height = 250;
+      return;
+    }
+
+  if(viewPortHeight < 100)
+    viewPortHeight = 100;
+  if(viewPortWidth < 100)
+    viewPortWidth = 100;  
+  
+  // Shrink to .9* the canvas size, if needed
+  if(scale * width > .9 * viewPortWidth)
+    scale = .9 * viewPortWidth / width;
+  if(scale * height > .9 * viewPortHeight)
+    scale = .9 * viewPortHeight / height;
+
+  // Set the width of the scaled image
+  m_height = (int) (scale * height);
+  m_width  = (int) (scale * width);
+}
---
+++ src/Image.h
@@ -0,0 +1,122 @@
+// -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode: nil -*-
+//
+//  Copyright (C) 2004-2015 Andrej Vodopivec <andrej.vodopivec@gmail.com>
+//            (C) 2014-2015 Gunter Königsmann <wxMaxima@physikbuch.de>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+
+#ifndef IMAGE_H
+#define IMAGE_H
+
+#include "MathCell.h"
+#include <wx/image.h>
+
+#include <wx/filesys.h>
+#include <wx/fs_arc.h>
+#include <wx/buffer.h>
+
+/*! Manages an auto-scaling image
+
+  This class keeps two versions of an image:
+    - The image in its original compressed format. This way the image can losslessly
+      be exported lateron
+    - A bitmap version of the image that is scaled down to a size that makes sense 
+      with the current viewport.
+
+  Storing images this way has many advantages:
+    - It allows us to restrict scaling operations to only once on actually drawing 
+      the image after a resize of the wxMaxima window.
+    - We can keep (and therefore export and save) jpeg photos in their original 
+      format instead of using the .png compression that is superior for line art - 
+      but not for depicting real-live images.
+    - We save time on saving the .wxmx file since image compression, if done good, 
+      might need a good deal of time and this class never needs to compress images
+      itself.
+    - It allows images to keep their metadata, if needed
+    - and if we have big images (big plots or for example photographs) we don't need
+      to store them in their uncompressed form.
+    - One could even delete the cached scaled images for all cells that currently 
+      are off-screen in order to save memory.
+ */
+class Image
+{
+public:
+  Image();
+  /*! A constructor that loads a bitmap
+
+    This constructor actually has to do some compression since we got
+    the bitmap in an uncompressed form.
+   */
+  Image(wxBitmap bitmap);
+  /*! A constructor that loads an image
+
+    \param image The name of the file
+    \param filesystem The filesystem to load it from
+    \param viewport_xsize The width of the viewport the image is to be displayed in
+    \param viewport_ysize The height of the viewport the image is to be displayed in
+    \param remove true = Delete the file after loading it
+   */
+  Image(wxString image,bool remove = true, wxFileSystem *filesystem = NULL);
+  /*! Temporarily forget the scaled image in order to save memory
+
+    Will recreate the scaled image as soon as needed.
+   */
+  void ClearCache() {m_scaledBitmap.Create (0,0);}
+  //! Reads the compressed image into a memory buffer
+  wxMemoryBuffer ReadCompressedImage(wxInputStream *data);
+  //! Returns the file name extension of the current image
+  wxString GetExtension() {return m_extension;};
+  //! Loads an image from a file
+  void LoadImage(wxString image, bool remove = true);
+  //! "Loads" an image from a bitmap
+  void LoadImage(wxBitmap bitmap);
+  //! Saves the image in its original form, or as .png if it originates in a bitmap
+  wxSize ToImageFile(wxString filename);
+  //! Returns the bitmap being displayed
+  wxBitmap GetBitmap();
+  //! Returns the image in its unscaled form
+  wxBitmap GetUnscaledBitmap();
+  //! Needs to be called on changing the viewport size 
+  void ViewportSize(size_t viewPortWidth,size_t viewPortHeight,double scale);
+  //! The width of the scaled image
+  size_t m_width;
+  //! The height of the scaled image
+  size_t m_height;
+  //! Returns the original image in its compressed form
+  wxMemoryBuffer GetCompressedImage(){return m_compressedImage;}
+protected:
+  //! The width of the unscaled image
+  size_t m_originalWidth;
+  //! The height of the unscaled image
+  size_t m_originalHeight;
+  //! The current viewport scale
+  double m_scale;
+  //! The current viewport width
+  size_t m_viewportWidth;
+  //! The current viewport height
+  size_t m_viewportHeight;
+  //! The image in its original compressed form
+  wxMemoryBuffer m_compressedImage;
+  //! The bitmap, scaled down to the screen size
+  wxBitmap m_scaledBitmap;
+  //! The filesystem the image was taken from
+  wxFileSystem *m_fileSystem;
+  //! The file extension for the current image type
+  wxString m_extension;
+};
+
+#endif // IMAGE_H
