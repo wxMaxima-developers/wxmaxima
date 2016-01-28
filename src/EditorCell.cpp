742d24c0fe858ede558ba20dd9aec7780dfa0337
Gunter Königsmann <gunter@peterpall.de>
2016-01-28 22:24:04 +0100

    TeX export: Did get the @ sign wrong.

--- src/EditorCell.cpp
+++ src/EditorCell.cpp
@@ -243,7 +243,7 @@ wxString EditorCell::ToTeX()
   text.Replace(wxT("$"), wxT("\\$"));
   text.Replace(wxT("%"), wxT("\\%"));
   text.Replace(wxT("&"), wxT("\\&"));
-  text.Replace(wxT("@"), wxT("\\mbox{\\verb+@+}"));
+  text.Replace(wxT("@"), wxT("\\ensuremath{@}"));
   text.Replace(wxT("#"), wxT("\\ensuremath{\\neq}"));
   text.Replace(wxT("\xDCB6"), wxT("~")); // A non-breakable space
   text.Replace(wxT("<"), wxT("\\ensuremath{<}"));
