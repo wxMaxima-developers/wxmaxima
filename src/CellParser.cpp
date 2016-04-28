1f52f46c93654b530b3b7317c10b4dacb0ad6cc5
Gunter Königsmann <gunter@peterpall.de>
2016-04-28 20:50:53 +0200

    A default color for text that coincides with the selection that doesn't nearly coincide with its color, too. Resolves #709

--- src/CellParser.cpp
+++ src/CellParser.cpp
@@ -350,7 +350,7 @@ void CellParser::ReadStyle()
 #endif
   if (config->Read(wxT("Style/Selection/color"),
                    &tmp)) m_styles[TS_SELECTION].color.Set(tmp);
-  m_styles[TS_EQUALSSELECTION].color = wxT("rgb(192,192,255)");
+  m_styles[TS_EQUALSSELECTION].color = wxT("rgb(192,255,192)");
   if (config->Read(wxT("Style/EqualsSelection/color"),
                    &tmp)) m_styles[TS_EQUALSSELECTION].color.Set(tmp);
 
