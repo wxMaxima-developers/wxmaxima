900c722db71e45e30bb16049a0ea05d3fa5c35d4
Gunter Königsmann <gunter@peterpall.de>
2016-08-30 09:05:51 +0200

    Revert "More robustness if maxima outputs non-utf8 characters on stdout or stderr.": Works fine on Linux. But now the nightly windows build is broken and this is the only change that might have to do with it.
    This reverts commit 269717339f851a7d7bb74e8a65266b0afc770821.

--- src/wxMaxima.cpp
+++ src/wxMaxima.cpp
@@ -2581,27 +2581,24 @@ void wxMaxima::ReadStdErr()
   {
     wxASSERT_MSG(m_input != NULL,wxT("Bug: Trying to read from maxima but don't have a input stream"));
     wxTextInputStream istrm(*m_input);
-    wxString o = _("Message from the stdout of Maxima: ");
-    wxChar ch;
-    while ((ch = istrm.GetChar()) != 0) 
-      o += ch;
+    wxString o;
+    while (m_process->IsInputAvailable())
+      o += m_input->GetC();
 
     bool pollStdOut = false; 
     wxConfig *config = (wxConfig *)wxConfig::Get();
     config->Read(wxT("pollStdOut"), &pollStdOut);
      
     if(pollStdOut)
-      DoRawConsoleAppend(o, MC_TYPE_DEFAULT);
+      DoRawConsoleAppend(_("Message from the stdout of Maxima: ") + o, MC_TYPE_DEFAULT);
   }
   if(m_process->IsErrorAvailable())
   {
     wxASSERT_MSG(m_error!=NULL,wxT("Bug: Trying to read from maxima but don't have a error input stream"));
     wxString o = wxT("Message from maxima's stderr stream: ");
-    wxTextInputStream estrm(*m_error);
-    wxChar ch;
-    while ((ch = estrm.GetChar()) != 0) 
-      o += ch;
-    
+    while (m_process->IsErrorAvailable())
+      o += m_error->GetC();
+
     DoRawConsoleAppend(o, MC_TYPE_ERROR);
     
     // If maxima did output something it defintively has stopped.
