216d9b75f37b1e87a90c5531a185deb23106aa15
Gunter Königsmann <gunter@peterpall.de>
2017-05-30 10:01:33 +0200

    Documented the notification class.

--- src/Notification.h
+++ src/Notification.h
@@ -34,26 +34,43 @@
 #include <wx/notifmsg.h>
 #include <wx/wx.h>
 
+/*! A user notification the operating system claims to be non-disturbing
+
+  We use this notification sparingly, which means: Only if the wxMaxima window has lost
+  focus between the user action that caused maxima to run a command and the moment the 
+  command has finished or has caused an error.
+ */
 class Notification: public wxNotificationMessage
 {
 public:
+  //! A constructor that doesn't take any arguments
   Notification();
+  //! A constructor that completely prepares the notification, but doesn't call Show() yet.
   Notification(const wxString &title,
                const wxString &message=wxEmptyString,
                wxWindow *parent=NULL,
                int flags=wxICON_INFORMATION);
+  //! Makes the notification appear.
   virtual void Show();
+  //! Informs the notification which the main window is it notified for.
   virtual void SetParent(wxWindow *parent);
+  //! Returns a pointer to the main window or NULL, if no main window is set.
   virtual wxWindow *GetParent(){return m_parent;}
+  //! Tell the operating system that the notification may be closed.
   virtual bool Close();
+  //! Might produce false positives, but at least tries to determine if the notification is active.
   bool IsShown(){return m_shown;}
   //! The cell we signal an error for
   GroupCell *m_errorNotificationCell;
 private:
+  //! May this notification be currently visible?
   bool m_shown;
+  //! The main window we notify for.
   wxWindow *m_parent;
 protected:
+  //! Called on clicking at the notification, if the OS supports that.
   void OnClick(wxCommandEvent &event);
+  //! Called on closing the notification by the OS or user action, if supported.
   void OnDismissed(wxCommandEvent &event);
 };
 
