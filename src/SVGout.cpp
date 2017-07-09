0c1519f4aaa2b54eebd161d6d86b75d2dd2470fd
Gunter Königsmann <gunter@peterpall.de>
2017-07-09 18:05:07 +0200

    Correct scaling of the svg output at zoom factors != 1.00

--- src/SVGout.cpp
+++ src/SVGout.cpp
@@ -68,6 +68,7 @@ bool Svgout::Layout(long int maxSize)
 
   *m_configuration = new Configuration(*m_dc);
   (*m_configuration)->SetClientWidth(BM_FULL_WIDTH);
+  (*m_configuration)->SetZoomFactor(1);
   // The last time I tried it the vertical positioning of the elements
   // of a big unicode parenthesis wasn't accurate enough in svg to be
   // usable. Also the probability was high that the right font wasn't
