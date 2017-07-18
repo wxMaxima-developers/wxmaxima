b2020c899f55a67ebb521977c22699a7d342da06
Gunter Königsmann <gunter@peterpall.de>
2017-07-18 08:12:00 +0200

    Extra paranoia.

--- src/ParenCell.cpp
+++ src/ParenCell.cpp
@@ -203,6 +203,9 @@ void ParenCell::RecalculateWidths(int fontsize)
         m_signWidth = signWidth2;
       if(m_signWidth < signWidth3)
         m_signWidth = signWidth3;
+
+      if(m_extendHeight < 1)
+        m_extendHeight = 1
       m_numberOfExtensions = ((size - m_signTopHeight - m_signBotHeight + m_extendHeight/ 2 - 1) / m_extendHeight);
       if(m_numberOfExtensions < 0)
         m_numberOfExtensions = 0;
