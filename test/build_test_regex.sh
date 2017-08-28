cceff919dbf0c8b96c45ffe4fd86660042e582a5
Guglielmo Saggiorato <astyonax@gmail.com>
2017-08-28 10:00:19 +0200

    updated script to test wxmaxima

--- test/build_test_regex.sh
+++ test/build_test_regex.sh
@@ -1,8 +1,13 @@
-#sudo apt-get build-dep wxmaxima
+# author: Guglielmo Saggiorato
+# email: astyonax@gmail.com
+# date: 28.08.17
+# purpose: Build and run maxima to test the BetterTeX function.
+# Tought to be exectuted on a debian-based setup (with aptitude)
+
+#sudo aptitude build-dep wxmaxima
 mkdir build
 cd build
 cmake ..
 set -e
 cmake  --build .
 ./src/wxmaxima ../test/regex.wxmx
-
