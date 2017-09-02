5350672dcd671fc9d18716504737a0e14aa5bce0
Guglielmo Saggiorato <astyonax@gmail.com>
2017-09-02 08:16:53 +0200

    edge cases, no debug, limited while loop

--- src/BetterTeX.cpp
+++ src/BetterTeX.cpp
@@ -28,30 +28,36 @@
 #include "BetterTeX.h"
 #include <wx/regex.h>
 
-wxString ProtectedReplace(wxString str, wxString expr, wxString replacement)
+// #define RXDBG
+
+wxString ProtectedReplace(wxString str, wxString expr, wxString replacement,wxString watchout="")
 {
     // This implements a sort of look behind
     // to protect some keyword or context from being destroyed
 
-    wxString protection = "([\\\\frac|\\\\sqrt|}])";
+    wxString protection = "([\\\\frac|\\\\sqrt|}"+watchout+"])";
     wxRegEx re(protection+expr,wxRE_ADVANCED);
     wxRegEx re_go(expr,wxRE_ADVANCED);
 
     re.IsValid() && re.Matches(str);
     bool doit = true;
     wxString kw  = re.GetMatch(str,1);
-    wxLogMessage(":"+kw+":");
+    #ifdef RXDBG
+        wxLogMessage(":"+kw+":");
+    #endif
 
     doit = (kw.length()==0) && doit;
     // doit = doit || (keywords.find(kw) == wxNOT_FOUND);
 
     if (doit)
     {
-        wxLogMessage("inside");
+        #ifdef RXDBG
+            wxLogMessage("inside");
+        #endif
         re_go.Replace(&str,replacement);
-        wxRegEx spaces("\\s+([\\}\\{\\)\\(])",wxRE_ADVANCED);
+        wxRegEx spaces("\\s+([\\}\\{\\)\\(_^])",wxRE_ADVANCED);
         spaces.Replace(&str,"\\1");
-        wxRegEx spaces2("([\\}\\{\\)\\(])\\s+",wxRE_ADVANCED);
+        wxRegEx spaces2("([\\}\\{\\)\\(_^])\\s+",wxRE_ADVANCED);
         spaces2.Replace(&str,"\\1");
     }
     return str;
@@ -65,36 +71,52 @@ wxString BetterTeX(wxString str)
   // I document here few notes:
   // The implementation of wxRE_ADVANCED (or ARE in the docs) is from Henry Spencer,
   // that originally wrote `regex`. This is also the `tcl`  implementation.
-  // It's not exaclty ECMAScript compatible, so when wx will remove support
+  // It's not exaclty ECMAScript compatible, so when wxwidgets will remove support
   // for their/this regex.h, there will be some adjustments to use the C++11
   // regex implementation.
   wxString retval = str;
-  wxLogMessage("0 " + retval);
+  #ifdef RXDBG
+    wxLogMessage("0 " + retval);
+  #endif
   // PART A --IMHO useless keywords
   wxRegEx mathit(wxT("\\\\mathit{(\\w+?)}"),wxRE_ADVANCED);
   mathit.Replace(&retval,"\\1");
   wxRegEx ensuremath(wxT("\\\\ensuremath{([\\w\\\\]+?)}"),wxRE_ADVANCED);
   ensuremath.Replace(&retval,"\\1");
-  wxLogMessage("A " + retval);
-  // END PART A
+  #ifdef RXDBG
+    wxLogMessage("A " + retval);
+  #endif
   // PART B --HANDLES CASES super  {{str}^{str}} and sub {}_{} scripts
   // intercept braces around single char
-  retval = ProtectedReplace(retval,"{([a-zA-Z0-9])}","\\1");
-  wxLogMessage("B " + retval);
+  retval = ProtectedReplace(retval,"{([a-zA-Z0-9])}"," \\1 ");
+  #ifdef RXDBG
+    wxLogMessage("B " + retval);
+  #endif
   // {str}^{str}->str^{str}
-  retval = ProtectedReplace(retval,wxT("{([^}{\\(\\))]+?)}[\\^_]"),"\\1^");
-  wxLogMessage("C " + retval);
+  // {([^}{\\(\\)]+?)}[\\^_]
+  retval = ProtectedReplace(retval,wxT("{([^}{\\(\\)]+?)}[\\^]"),"\\1^","_");
+  retval = ProtectedReplace(retval,wxT("{([^}{\\(\\)]+?)}[\\_]"),"\\1_","^");
+  // match single char again (is there some sort of unpredictable behavior??)
+  retval = ProtectedReplace(retval,wxT("{([^}{\\(\\)])}[\\^]"),"\\1^");
+  retval = ProtectedReplace(retval,wxT("{([^}{\\(\\)])}[\\_]"),"\\1_");
+  #ifdef RXDBG
+    wxLogMessage("C " + retval);
+  #endif
   // intercepts {a^b}
   retval = ProtectedReplace(retval,wxT("{(\\w[\\^_]\\w)}"),"\\1");
-  wxLogMessage("D " + retval);
+  #ifdef RXDBG
+    wxLogMessage("D " + retval);
+  #endif
   // intercepts {a^{str}} , but not \sqrt{a^b}, or \frac{a^b}{c}
   retval = ProtectedReplace(retval,wxT("{([^}{]+?[\\^_]{[^}{]+?})}"),"\\1");
-
   // END PART B
 
   // PART C --HANDLES Parenteresis as in \left ..\right..
-  wxLogMessage("E " + retval);
+  #ifdef RXDBG
+   wxLogMessage("E " + retval);
+  #endif
   wxString tmp;
+  int countloop=0;
   do{
     tmp=retval;
     // intercepts {\left(..\right)} -- protected if begins with c{..} or }{} as would be in \frac{..}{..}
@@ -102,9 +124,11 @@ wxString BetterTeX(wxString str)
     retval = ProtectedReplace(retval,"{(\\\\\\w+?\\\\left.+?\\\\right.)}","\\1");
     // intercepts {\left(..\right)^{str}} -- protected if begins with c{..} or }{} as would be in \frac{..}{..}
     retval = ProtectedReplace(retval,wxT("{(\\\\left\\(.+?\\\\right\\)[\\^_]{[^}{]+?})}"),"\\1");
+    countloop+=1;
   }
-  while (retval!=tmp);
-  wxLogMessage("--> "+retval);
-  // TODO: handle \left[ \right], or || etc
+  while ((retval!=tmp) && countloop < 2);
+  #ifdef RXDBG
+    wxLogMessage("--> "+retval);
+  #endif
   return retval;
 }
