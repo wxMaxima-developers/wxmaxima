bf4c85b6985105c3871322d8a34541350df73f57
Gunter Königsmann <gunter@peterpall.de>
2017-10-10 19:09:04 +0200

    Two more tooltips.

--- src/TextCell.cpp
+++ src/TextCell.cpp
@@ -150,11 +150,11 @@ void TextCell::SetValue(const wxString &text)
           isinum = false;
           break;
         }
-
+      
       if(isinum)
         m_toolTip = _("An integration constant.");
     }
-}
+  }
   
   if (m_textStyle == TS_NUMBER)
   {
@@ -172,6 +172,9 @@ void TextCell::SetValue(const wxString &text)
   }
   else
   {
+    if(text.StartsWith(wxT("incorrect syntax")) && (text.Contains(wxT("is not an infix operator"))))
+      m_toolTip = _("A command or number wasn't preceded by a \":\", a \"$\", a \";\" or a \",\".\n"
+        "Most probable cause: A missing comma between two list items.");
     if(text.StartsWith(wxT("part: fell off the end.")))
        m_toolTip = _("part() or the [] operator was used in order to extract the nth element "
                      "of something that was less than n elements long.");
