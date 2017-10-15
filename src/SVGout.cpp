e8692165d89baf2d5fcf5d259b96dbef21a077a7
Gunter Königsmann <gunter@peterpall.de>
2017-10-15 17:28:13 +0200

    If that was the bug I am searching?

--- src/SVGout.cpp
+++ src/SVGout.cpp
@@ -52,7 +52,7 @@ Svgout::Svgout(Configuration **configuration, wxString filename, double scale)
   *m_configuration = new Configuration(*m_dc);
   (*m_configuration)->ShowCodeCells(m_oldconfig->ShowCodeCells());
   (*m_configuration)->SetClientWidth(10000*m_scale);
-  (*m_configuration)->SetZoomFactor(m_scale);
+  (*m_configuration)->SetZoomFactor_temporarily(m_scale);
   // The last time I tried it the vertical positioning of the elements
   // of a big unicode parenthesis wasn't accurate enough in svg to be
   // usable. Also the probability was high that the right font wasn't
