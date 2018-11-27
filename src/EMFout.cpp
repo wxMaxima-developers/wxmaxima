f7b8ac5dd22b04da20e8661def729371b1c24f54
Gunter Königsmann <gunter@peterpall.de>
2018-11-27 19:05:53 +0100

    Unbroke the windows build.

--- src/EMFout.cpp
+++ src/EMFout.cpp
@@ -60,7 +60,7 @@ Emfout::Emfout(Configuration **configuration, wxString filename)
   // usable. Also the probability was high that the right font wasn't
   // available in inkscape.
   (*m_configuration)->SetGrouphesisDrawMode(Configuration::handdrawn);
-  m_configuration->Printing(true);
+  (*m_configuration)->Printing(true);
   (*m_configuration)->SetForceUpdate(true);
 }
 
