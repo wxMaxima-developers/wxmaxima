079fc4707e1589796efea8c1507e6d8822d8db9d
Gunter Königsmann <gunter@peterpall.de>
2019-08-23 21:08:57 +0200

    Removed my debug code.

--- src/wxMaxima.cpp
+++ src/wxMaxima.cpp
@@ -757,7 +757,6 @@ void wxMaxima::SendMaxima(wxString s, bool addToHistory)
         // Append everything except the NULL char at the end
         m_rawDataToSend.AppendData(data_raw.data(),data_raw.length());
         m_client->Write((void *)m_rawDataToSend.GetData(), m_rawDataToSend.GetDataLen());
-        std::cerr<<"sent="<<s;
       }
       
       if (m_client->Error()) {
