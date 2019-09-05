cff62e9c75fdfe8fe1d12fc5957016e3ae7e4d3a
Gunter Königsmann <gunter@peterpall.de>
2019-09-05 11:00:55 +0200

    Another try to make the tests work again.

--- test/CTestCustom.cmake
+++
@@ -1,9 +0,0 @@
-find_program(DESKTOP_FILE_VALIDATE_FOUND desktop-file-validate)
-if(NOT DESKTOP_FILE_VALIDATE_FOUND)
-  list(APPEND CTEST_CUSTOM_TESTS_IGNORE check_desktop_file)
-endif()
-
-find_program(APPSTREAM_UTIL_FOUND appstream-util)
-if(NOT APPSTREAM_UTIL_FOUND)
-  list(APPEND CTEST_CUSTOM_TESTS_IGNORE check_appstream_file)
-endif()
