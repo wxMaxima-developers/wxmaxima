da90e8de99e2d900601e475da983b00df2d62f44
Gunter Königsmann <gunter@peterpall.de>
2019-09-19 19:43:50 +0200

    I guess this was a wxWidgets bug but we now correctly find our demo files.

--- src/Autocomplete.cpp
+++ src/Autocomplete.cpp
@@ -4732,9 +4732,8 @@ bool AutoComplete::LoadSymbols()
 
   // Prepare a list of all built-in demos of maxima.
   {
-    wxFileName demoDir(m_configuration->MaximaShareDir());
+    wxFileName demoDir(m_configuration->MaximaShareDir()+"/..");
     demoDir.MakeAbsolute();
-    demoDir.RemoveLastDir();
     GetDemoFiles_includingSubdirs maximaLispIterator (m_builtInDemoFiles);
     wxLogMessage(
       wxString::Format(
