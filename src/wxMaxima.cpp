e97e952ebae559a6ff93d4105c4902721345bb7f
Gunter Königsman <gunterkoenigsmann@users.noreply.github.com>
2019-11-06 08:47:42 +0100

    Corrected the save-on-close logic
    Resolves #1168

--- src/wxMaxima.cpp
+++ src/wxMaxima.cpp
@@ -8734,7 +8734,7 @@ int wxMaxima::SaveDocumentP()
   }
   else
   {
-    if (m_worksheet->m_configuration->AutoSaveAsTempFile())
+    if (!m_worksheet->m_configuration->AutoSaveAsTempFile())
     {
       if (SaveFile())
         return wxID_NO;
