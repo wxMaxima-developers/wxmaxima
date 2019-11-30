0aa30374274180523da16092a387438142a2ce5d
Gunter Königsmann <gunter@peterpall.de>
2019-11-30 12:20:14 +0100

    Documentation and ran the test suite.

--- src/Autocomplete_Builtins.cpp
+++ src/Autocomplete_Builtins.cpp
@@ -1,3 +1,32 @@
+// -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode: nil -*-
+//
+//  Copyright (C) 2009-2015 Andrej Vodopivec <andrej.vodopivec@gmail.com>
+//  Copyright (C) 2015-2019 Gunter Königsmann    <wxMaxima@physikbuch.de>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  SPDX-License-Identifier: GPL-2.0+
+
+/*! \file
+  This file tells wxMaxima about known builtin commands of maxima.
+
+  The list of builtin commands was long enough to justify splitting it into
+  a separate file.
+*/
+
 #include "Autocomplete.h"
 
 bool AutoComplete::LoadBuiltinSymbols()
