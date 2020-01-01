7fd6fa26e9e9b9660bae5c0663201b14bb27b521
Gunter Königsmann <gunter@peterpall.de>
2020-01-01 19:06:30 +0100

    wxplot_usesvg wasn't part of the autocompletion.

--- src/Autocomplete_Builtins.cpp
+++ src/Autocomplete_Builtins.cpp
@@ -4551,6 +4551,7 @@ bool AutoComplete::LoadBuiltinSymbols()
   m_wordList[command].Add(wxT("wxanimate_framerate"));
   m_wordList[command].Add(wxT("wxanimate_autoplay"));
   m_wordList[command].Add(wxT("wxplot_pngcairo"));
+  m_wordList[command].Add(wxT("wxplot_usesvg"));
   m_wordList[command].Add(wxT("set_display"));
   m_wordList[command].Add(wxT("wxplot2d"));
   m_wordList[tmplte].Add(wxT("wxplot2d(<expr>,<x_range>)"));
