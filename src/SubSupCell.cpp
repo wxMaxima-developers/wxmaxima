da08648c4d33bffad4568a3286b0651028853004
Gunter Königsmann <gunter@peterpall.de>
2020-01-11 17:54:35 +0100

    A try to make Robert's example display correctly.

--- src/SubSupCell.cpp
+++ src/SubSupCell.cpp
@@ -199,7 +199,6 @@ void SubSupCell::Draw(wxPoint point)
     if(m_preSubCell)
     {
       wxPoint presub = point;
-      presub.x -= m_preSubCell->GetFullWidth();
       presub.y += m_baseCell->GetMaxDrop() +
         m_preSubCell->GetCenterList() -
         Scale_Px(.8 * m_fontSize + MC_EXP_INDENT);
@@ -209,13 +208,13 @@ void SubSupCell::Draw(wxPoint point)
     if(m_preSupCell)
     {
       wxPoint presup = point;
-      presup.x -= m_preSupCell->GetFullWidth();
       presup.y -= m_baseCell->GetCenterList() - m_preSupCell->GetHeightList()
         + m_preSupCell->GetCenterList() +
         Scale_Px(.8 * m_fontSize + MC_EXP_INDENT);;
       m_preSupCell->DrawList(presup);
     }
-    
+
+    point.x += preWidth;
     m_baseCell->DrawList(point);
 
     if(m_postSubCell)
