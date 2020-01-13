0136aa1fc6761cf86da670f23bc715c76e87c0b3
Gunter Königsmann <gunter@peterpall.de>
2020-01-13 07:25:09 +0100

    Export of pre-{sub/sup}cells to OOML

--- src/SubSupCell.cpp
+++ src/SubSupCell.cpp
@@ -447,7 +447,19 @@ wxString SubSupCell::ToMathML()
 }
 wxString SubSupCell::ToOMML()
 {
-  wxString retval = wxT("<m:sSubSup><m:e>") +
+  wxString retval;
+  if(m_preSupCell || m_preSubCell)
+  {
+    retval += wxT("<m:sSubSup><m:e>") +
+      wxT("</m:e><m:sup>");
+    if(m_preSubCell)
+      retval += m_preSubCell->ListToOMML();
+    retval += wxT("</m:sup><m:sub>");
+    if(m_preSupCell)
+      retval += m_preSupCell->ListToOMML();
+    retval += wxT("</m:sub></m:sSubSup>\n");
+  }
+  retval += wxT("<m:sSubSup><m:e>") +
     m_baseCell->ListToOMML() + wxT("</m:e><m:sup>");
   if(m_postSubCell)
     retval += m_postSubCell->ListToOMML();
