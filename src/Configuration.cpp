wxMaxima-developers/wxmaxima

commit 32694d4790fcecad127df683b219fd05276878db

i18n: Changed wxFileConfig to wxConfig, so that Windows and Unix should work.
For Unix wxConfig is an alias for wxFileConfig, but for Windows
usually wxRegConfig (store information in the registry).

(Changes for working i18n are not finished, more to come...)

src/Configuration.cpp
@@ -745,7 +745,7 @@ void Configuration::ReadStyles(wxString file)
   else
   {
     wxFileInputStream str(file);
-    config = new wxFileConfig(str);
+    config = new wxConfig(str);
   }
   
   // Font
@@ -821,7 +821,7 @@ void Configuration::WriteStyles(wxString file)
   if (file == wxEmptyString)
     config = wxConfig::Get();
   else
-    config = new wxFileConfig(wxT("wxMaxima"),wxEmptyString,file);
+    config = new wxConfig(wxT("wxMaxima"), wxEmptyString, file);
 
   // Font
   config->Write(wxT("Style/fontname"), m_fontName);

src/main.cpp
@@ -97,7 +97,7 @@ bool MyApp::OnInit()
   #endif
   #endif
 
-  wxConfig::Set(new wxFileConfig(wxT("wxMaxima"), wxEmptyString, m_configFileName));
+  wxConfig::Set(new wxConfig(wxT("wxMaxima"), wxEmptyString, m_configFileName));
 
   m_locale.AddCatalogLookupPathPrefix(m_dirstruct.LocaleDir());
   m_locale.AddCatalogLookupPathPrefix(m_dirstruct.LocaleDir() + wxT("/wxwin"));
@@ -212,9 +212,8 @@ bool MyApp::OnInit()
     wxFileName configFile(ini);
     configFile.MakeAbsolute();
     Configuration::m_configfileLocation_override = configFile.GetFullPath();
-    wxConfig::Set(new wxFileConfig(wxT("wxMaxima"),
-                                   wxEmptyString,
-                                   Configuration::m_configfileLocation_override));
+    wxConfig::Set(new wxConfig(wxT("wxMaxima"), wxEmptyString,
+                               Configuration::m_configfileLocation_override));
   }
   else
     wxConfig::Set(new wxConfig(wxT("wxMaxima")));

src/wxMaximaFrame.cpp
@@ -1438,8 +1438,8 @@ void wxMaximaFrame::ReReadConfig()
     {
       wxLogMessage(wxString::Format(_("Re-Reading the config from %s."),
                      Configuration::m_configfileLocation_override.utf8_str()));
-      wxConfig::Set(new wxFileConfig(wxT("wxMaxima"),
-                                     wxEmptyString, Configuration::m_configfileLocation_override));
+      wxConfig::Set(new wxConfig(wxT("wxMaxima"),
+                                 wxEmptyString, Configuration::m_configfileLocation_override));
     }
   }
   #endif

commit ce208edb06d4fdb868a04af7ac9ac8370c15663f

Fix Windows compilation error introduced with commit 32694d4.
For exporting and importing Font style files wxFileConfiguration
is needed.

src/Configuration.cpp
@@ -745,7 +745,7 @@ void Configuration::ReadStyles(wxString file)
   else
   {
     wxFileInputStream str(file);
-    config = new wxConfig(str);
+    config = new wxFileConfig(str);
   }
   
   // Font
@@ -821,7 +821,7 @@ void Configuration::WriteStyles(wxString file)
   if (file == wxEmptyString)
     config = wxConfig::Get();
   else
-    config = new wxConfig(wxT("wxMaxima"), wxEmptyString, file);
+    config = new wxFileConfig(wxT("wxMaxima"), wxEmptyString, file);
 
   // Font
   config->Write(wxT("Style/fontname"), m_fontName);