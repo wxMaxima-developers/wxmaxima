16ebbb236c9352c9e982355e808019b0b5047f00
Gunter Königsmann <gunter@peterpall.de>
2020-02-11 06:56:57 +0100

    SpeedUp: Only unzip wxMathML.lisp, if necessary.

--- src/wxMathml.cpp
+++ src/wxMathml.cpp
@@ -9,21 +9,24 @@
 
 wxMathML::wxMathML()
 {
-  // Unzip wxMathml.lisp: We need to store it in a .zip format
-  // in order to avoid a bug in the ArchLinux C compiler that
-  // seems to replace long strings by a "\0".
-  wxMemoryInputStream istream(wxMathML_lisp_gz, wxMathML_lisp_gz_len);
-
-  wxZlibInputStream zstream(istream);
-  wxTextInputStream textIn(zstream);
-  wxString line;
-  
-  while(!istream.Eof())
-  {
-    line = textIn.ReadLine();
-    m_wxMathML += line + wxT("\n");
-  }
-  wxASSERT_MSG(m_wxMathML.Length()>64000,_("Compiler-Bug? wxMathml.lisp is shorter than expected!"));
+  if(m_maximaCMD.IsEmpty())
+    {
+      // Unzip wxMathml.lisp: We need to store it in a .zip format
+      // in order to avoid a bug in the ArchLinux C compiler that
+      // seems to replace long strings by a "\0".
+      wxMemoryInputStream istream(wxMathML_lisp_gz, wxMathML_lisp_gz_len);
+      
+      wxZlibInputStream zstream(istream);
+      wxTextInputStream textIn(zstream);
+      wxString line;
+      
+      while(!istream.Eof())
+	{
+	  line = textIn.ReadLine();
+	  m_wxMathML += line + wxT("\n");
+	}
+      wxASSERT_MSG(m_wxMathML.Length()>64000,_("Compiler-Bug? wxMathml.lisp is shorter than expected!"));
+    }
 }
 
 wxString wxMathML::GetCmd()
