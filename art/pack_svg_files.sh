781c1bd377978c36e26193320f8c79fe7f143506
Wolfgang Dautermann <dauti@users.sourceforge.net>
2020-04-25 12:45:27 +0200

    The FSF has a new address, fixed that in our code files.

--- art/pack_svg_files.sh
+++ art/pack_svg_files.sh
@@ -6,10 +6,10 @@ for d in config statusbar toolbar draw; do
     cd "$d" || exit
     cat >images.h <<END
 /* Automatically generated file using pack_svg_files.sh                                 */
-/* SVG images are compressed using gzupand converted to C using xxd                     */
+/* SVG images are compressed using gzip and converted to C using xxd                    */
 /* This file is part of wxMaxima.                                                       */
 
-/* Copyright (C) 2018 wxMaxima Team (https://wxMaxima-developers.github.io/wxmaxima/)               */
+/* Copyright (C) 2020 wxMaxima Team (https://wxMaxima-developers.github.io/wxmaxima/)   */
 
 /* This program is free software; you can redistribute it and/or modify                 */
 /* it under the terms of the GNU General Public License as published by                 */
@@ -23,7 +23,7 @@ for d in config statusbar toolbar draw; do
 
 /* You should have received a copy of the GNU General Public License                    */
 /* along with this program; if not, write to the Free Software                          */
-/* Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA            */
+/* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA         */
 END
     echo "Dir $d"
     for i in *.svg; do
