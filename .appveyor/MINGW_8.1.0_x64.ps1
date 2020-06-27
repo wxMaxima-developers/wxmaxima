03c9669d36532bbca7de7d1abc96c5f761776b5f
Kuba Ober <kuba@bertec.com>
2020-06-27 01:26:44 -0400

    Add MinGW 8.1.0 build.

---
+++ .appveyor/MINGW_8.1.0_x64.ps1
@@ -0,0 +1,7 @@
+# A script that pulls in MinGW 8.1.0 variables
+echo '- Setting environment variables for MinGW 8.1.0'
+
+$env:PATH = "C:\mingw-w64\x86_64-8.1.0-posix-seh-rt_v6-rev0\mingw64\bin;C:\msys64\usr\bin;${env:PATH}"
+
+$env:CC = "x86_64-w64-mingw32-gcc.exe"
+$env:CXX = "x86_64-w64-mingw32-g++.exe"
