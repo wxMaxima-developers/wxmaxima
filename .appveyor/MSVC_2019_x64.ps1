75ca61e7c493e5adc55e1b007186ec8f91520c31
Kuba Ober <kuba@bertec.com>
2020-06-27 01:44:44 -0400

    Cache the environment generated by vcvars.

--- .appveyor/MSVC_2019_x64.ps1
+++ .appveyor/MSVC_2019_x64.ps1
@@ -1,7 +1,10 @@
 # A script that pulls in Visual Studio variables
 echo '- Setting environment variables for Visual Studio'
-cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
-Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
+$ENV_LIST = "$env:temp\vcvars.txt"
+if (!(Test-Path $ENV_LIST)) {
+   cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat`" && set > $ENV_LIST"
+}
+Get-Content "$ENV_LIST" | Foreach-Object {
   if ($_ -match "^(.*?)=(.*)$") {
     Set-Content "env:\$($matches[1])" $matches[2]
   }
