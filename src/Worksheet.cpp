08153e587c219fc856eca46600a328b276bfad3d
Gunter Königsman <gunterkoenigsmann@users.noreply.github.com>
2020-07-25 10:16:25 +0200

    A missing closing parenthesis

--- src/Worksheet.cpp
+++ src/Worksheet.cpp
@@ -8024,7 +8024,7 @@ bool Worksheet::Autocomplete(AutoComplete::autoCompletionType type)
     int width;
     int height;
     GetClientSize(&width, &height);
-    if((pos.x >= 0) && (pos.y >= 0)
+    if((pos.x >= 0) && (pos.y >= 0))
        m_autocompletePopup -> SetPosition(pos);
     m_autocompletePopup -> Create(this);
     m_autocompletePopup -> SetFocus();
