4dce69757278d6e57a72bec3dccc87e79bd53916
Kuba Ober <kuba@bertec.com>
2020-08-14 12:38:53 -0400

    Fix the font size defaulting logic for TextStyle configuration reading.
    This also gets rid of the registry read error message on Windows.

--- src/TextStyle.cpp
+++ src/TextStyle.cpp
@@ -506,8 +506,8 @@ static const wxString k_italic = wxT("%s/italic");
 static const wxString k_slant = wxT("%s/slant");
 static const wxString k_underlined = wxT("%s/underlined");
 static const wxString k_strikethrough = wxT("%s/strikethrough");
-static const wxString k_fontsize = wxT("%s/Style/Text/fontsize_float");
-static const wxString k_fontsize_long = wxT("%s/Style/Text/fontsize");
+static const wxString k_fontsize_float = wxT("%s/Style/Text/fontsize_float");
+static const wxString k_fontsize_legacy = wxT("%s/Style/Text/fontsize");
 static const wxString k_fontname = wxT("%s/Style/Text/fontname");
 
 Style &Style::Read(wxConfigBase *config, const wxString &where)
@@ -528,11 +528,9 @@ Style &Style::Read(wxConfigBase *config, const wxString &where)
   else if (config->Read(wxString::Format(k_slant, where), &tmpBool) && tmpBool) SetSlant(true);
   if (config->Read(wxString::Format(k_underlined, where), &tmpBool)) SetUnderlined(tmpBool);
   if (config->Read(wxString::Format(k_strikethrough, where), &tmpBool)) SetStrikethrough(tmpBool);
-  if (config->Read(wxString::Format(k_fontsize_long, where), &tmpLong))
-    SetFontSize(AFontSize(tmpLong));
-  if (config->Read(wxString::Format(k_fontsize, where), &tmpDouble))
+  if (config->Read(wxString::Format(k_fontsize_float, where), &tmpDouble))
     SetFontSize(AFontSize(tmpDouble));
-  if (config->Read(wxString::Format(k_fontsize, where), &tmpLong))
+  else if (config->Read(wxString::Format(k_fontsize_legacy, where), &tmpLong))
     SetFontSize(AFontSize(tmpLong));
   if (config->Read(wxString::Format(k_fontname, where), &tmpStr) && !tmpStr.empty())
     SetFontName(AFontName(tmpStr));
@@ -547,7 +545,7 @@ void Style::Write(wxConfigBase *config, const wxString &where) const
   config->Write(wxString::Format(k_bold, where), IsBold());
   config->Write(wxString::Format(k_italic, where), IsItalic());
   config->Write(wxString::Format(k_underlined, where), IsUnderlined());
-  config->Write(wxString::Format(k_fontsize, where), GetFontSize().Get());
+  config->Write(wxString::Format(k_fontsize_float, where), GetFontSize().Get());
   config->Write(wxString::Format(k_fontname, where), GetNameStr());
 
   // We don't write the slant, light nor strikethrough attributes so as not to grow the
