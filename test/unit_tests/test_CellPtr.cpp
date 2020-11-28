38141c0f9451bf04981a955ccf02cf8a9cb028cb
Kuba Ober <kuba@bertec.com>
2020-11-28 13:47:23 -0500

    Add a constexpr that g++-5 needs.

--- test/unit_tests/test_CellPtr.cpp
+++ test/unit_tests/test_CellPtr.cpp
@@ -33,7 +33,7 @@
 
 AFontSize Style::GetFontSize() const { return {}; }
 AFontName Style::Default_FontName() { return {}; }
-const AFontSize Style::Default_FontSize;
+constexpr const AFontSize Style::Default_FontSize;
 AFontSize Configuration::Scale_Px(AFontSize) const { return {}; }
 wxColour Configuration::GetColor(TextStyle) { return {}; }
 void Configuration::NotifyOfCellRedraw(const Cell *) {}
