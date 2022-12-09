3b70d8aaf3170f122bb1a5c528e666cd11902305
Gunter Königsmann <gunter@peterpall.de>
2022-12-09 20:17:12 +0100

    Saving boxes works now

--- src/cells/NamedBoxCell.cpp
+++ src/cells/NamedBoxCell.cpp
@@ -191,7 +191,7 @@ wxString NamedBoxCell::ToOMML() const {
 }
 
 wxString NamedBoxCell::ToXML() const {
-  wxString flags = " boxname =\"" + m_boxname->ListToXML() + "\"";
+  wxString flags = " boxname =\"" + m_boxname->GetValue() + "\"";
   if (HasHardLineBreak())
     flags += wxT(" breakline=\"true\"");
 
