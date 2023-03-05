5de2027b54cffca4a55f89737baa149537f3bbf4
Gunter Königsmann <gunter@peterpall.de>
2023-03-05 10:12:41 +0100

    A class that can provide bitmaps for us

---
+++ src/ArtProvider.cpp
@@ -0,0 +1,64 @@
+// -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode:
+// nil -*-
+//
+//  Copyright (C) 2023      Gunter Königsmann <wxMaxima@physikbuch.de>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+//
+//  SPDX-License-Identifier: GPL-2.0+
+
+/*\file
+
+  Generates all the bitmaps we need.
+*/
+
+#include <ArtProvider.h>
+#include <wx/bitmap.h>
+#include <wx/image.h>
+#include <wx/artprov.h>
+#include "SvgBitmap.h"
+#include "nanoSVG/nanosvg.h"
+#include "nanoSVG/nanosvgrast.h"
+
+wxBitmap ArtProvider::GetImage(wxWindow *win, wxString name, int width,
+			       unsigned char *data, size_t dataLen) {  
+  wxBitmap bmp = wxArtProvider::GetBitmap(name, wxART_TOOLBAR,
+                                          wxSize(width * 4, width * 4));
+  wxImage img;
+  
+  if (bmp.IsOk()) {
+    img = bmp.ConvertToImage();
+  }
+  if (img.IsOk())
+    {
+      img.Rescale(width, width, wxIMAGE_QUALITY_BICUBIC);
+#if defined __WXOSX__
+      bmp = wxBitmap(img, wxBITMAP_SCREEN_DEPTH, GetContentScaleFactor());
+#else
+      bmp = wxBitmap(img, wxBITMAP_SCREEN_DEPTH);
+#endif
+    }
+  if(!bmp.IsOk())
+    bmp = SvgBitmap(win, data, dataLen, width, width);
+
+  if(!bmp.IsOk())
+#if defined __WXOSX__
+    bmp = wxBitmap(wxSize(width, width), wxBITMAP_SCREEN_DEPTH, GetContentScaleFactor());
+#else
+    bmp = wxBitmap(wxSize(width, width), wxBITMAP_SCREEN_DEPTH);
+#endif
+  return bmp;
+}
---
+++ src/ArtProvider.h
@@ -0,0 +1,43 @@
+// -*- mode: c++; c-file-style: "linux"; c-basic-offset: 2; indent-tabs-mode: nil -*-
+//
+//  Copyright (C) 2004-2015 Andrej Vodopivec <andrej.vodopivec@gmail.com>
+//            (C) 2008-2009 Ziga Lenarcic <zigalenarcic@users.sourceforge.net>
+//            (C) 2012-2013 Doug Ilijev <doug.ilijev@gmail.com>
+//            (C) 2015-2016 Gunter Königsmann <wxMaxima@physikbuch.de>
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+//
+//  SPDX-License-Identifier: GPL-2.0+
+
+/*! \file
+  This file declares the class ToolBar that represents wxMaxima's main tool bar.
+*/
+
+#include <wx/bitmap.h>
+#include <wx/window.h>
+
+#ifndef _ARTPROVIDER_H
+#define _ARTPROVIDER_H
+
+class ArtProvider
+{
+public:
+  ArtProvider(){}
+  static wxBitmap GetImage(wxWindow *win, wxString name, int width, unsigned char *data,
+                           size_t dataLen);
+};
+
+#endif
