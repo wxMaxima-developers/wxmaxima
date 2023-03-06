145af006337b608acec609471ab108fafbbe6bfa
Gunter Königsmann <gunter@peterpall.de>
2023-03-06 19:40:33 +0100

    More documentation

--- src/wxMathML.lisp
+++ src/wxMathML.lisp
@@ -151,19 +151,19 @@
     #+clisp `(let ((custom:*suppress-check-redefinition* t)) ,form)
     #-(or sbcl clisp) `(progn ,form))
 
-  (defun read-wxmaxima-version (v)
-    (let* ((d1 (position #\. v))
-	   (year (subseq v 0 d1))
-	   (d2 (position #\. v :start (1+ d1)))
-	   (month (subseq v (1+ d1) d2))
-	   (rest (subseq v (1+ d2))))
-      (list '(mlist simp) (parse-integer year) (parse-integer month) rest)))
-
+  ;;; Any half-way new maxima will define these variables that add
+  ;;; info about the front-end to the build_info(). If we encounter
+  ;;; an old maxima that is no problem since we can create them
+  ;;; ourselves.
   (defvar $maxima_frontend nil)
   (defvar $maxima_frontend_version nil)
+  ;; The frontend name we know already. The frontend version is assigned
+  ;; using a :lisp command from the C++ program, instead.
   (setf (symbol-value '$maxima_frontend) "wxMaxima")
 
 
+  ;; Was mainly useful when maxima's build_info() didn't contain all that info,
+  ;; still
   (defun $wxbuild_info ()
     (progv  '(seconds minute hour day month year) cl-user:*maxima-build-time*
       (format t "wxMaxima version: ~a~%" $wxmaximaversion)
@@ -179,6 +179,9 @@
       (format t "Help dir: ~a~%" wxHelpDir))
     "")
 
+  ;; Tell the user where to report bugs.
+  ;; TODO: Make maxima's bug_report() extendible so we can add info about reporting
+  ;; frontend bugs here.
   (defmfun $wxbug_report ()
     (format t "wxMaxima is a graphical front end for Maxima, which does the mathematics in the background.~%")
     (format t "If you encounter a mathematical problem, it is probably a Maxima bug and should be submitted there.~%")
