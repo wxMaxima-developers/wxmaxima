b4a4d1e7ee76a7720bcba75b7857bddebec94cac
Gunter Königsmann <gunter@peterpall.de>
2023-03-09 08:56:15 +0100

    Compilation on MacOs

--- src/ArtProvider.cpp
+++ src/ArtProvider.cpp
@@ -60,13 +60,16 @@ wxBitmap ArtProvider::GetImage(wxWindow *win, wxString name, int width,
   if(!bmp.IsOk())
     bmp = SvgBitmap(win, data, dataLen, width, width);
 
-  if(!bmp.IsOk())
 #if defined __WXOSX__
-      int scaleFactor = win->GetContentScaleFactor();
-      if(scaleFactor < 1)
+  int scaleFactor = win->GetContentScaleFactor();
+  if(scaleFactor < 1)
 	scaleFactor = 1;
       if(scaleFactor > 16)
 	scaleFactor = 16;
+#endif
+
+  if(!bmp.IsOk())
+#if defined __WXOSX__
       
     bmp = wxBitmap(wxSize(width, width), wxBITMAP_SCREEN_DEPTH, scaleFactor);
 #else
--- src/ConfigDialogue.cpp
+++ src/ConfigDialogue.cpp
@@ -94,7 +94,9 @@ int ConfigDialogue::GetImageSize() {
 
 ConfigDialogue::ConfigDialogue(wxWindow *parent, Configuration *cfg)
   : wxPropertySheetDialog() {
-  Create(parent, wxID_ANY, _("wxMaxima configuration"), wxDefaultPosition,
+  Create(parent, wxID_ANY, _("wx  Create(parent, wxID_ANY, _("wxMaxima configuration"), wxDefaultPosition,
+         wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER);
+Maxima configuration"), wxDefaultPosition,
          wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER);
   m_maximaEnvDoc["MAXIMA_DEFAULT_LISP"] =
     _("If maxima versions compiled with different lisps are installed: The "
