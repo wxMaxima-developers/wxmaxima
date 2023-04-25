8e914f7b3bd6a533d6d4dcf761ceb9ca66c92ce4
alex-bel <alex.bel.izh@gmail.com>
2023-04-25 18:10:39 +0400

    EditorCell::FindMatchingQuotes(): check m_positionOfCaret bounds Fixes #1773

--- src/cells/EditorCell.cpp
+++ src/cells/EditorCell.cpp
@@ -2184,7 +2184,7 @@ bool EditorCell::FindMatchingQuotes() {
 
 void EditorCell::FindMatchingParens() {
   m_paren1 = m_paren2 = -1;
-  if (m_positionOfCaret < 0)
+  if (m_positionOfCaret < 0 || m_positionOfCaret >= m_text.Length())
     return;
 
   wxChar charUnderCursor = m_text.GetChar(m_positionOfCaret);
