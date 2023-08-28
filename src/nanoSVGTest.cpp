d16fef155c631d0ad1df4740579cf6f234b320c2
Gunter Königsman <gunterkoenigsmann@users.noreply.github.com>
2023-08-28 08:58:42 +0200

    Feature/koen gu/nano sv gtest (#1795)
    * Perhaps this is an automatic test if compiling in nanoSVG causes a linker error.
    
    If it doesn't we need to include it
    
    * A 2nd try to make a compile test with nanoSVG
    
    * Now we should automagically test if to include nanoSVG in our project.
    
    * Corrected the nanoSVG inclusion logic
    
    * A nanoSVG detector workaround for cygwin
    
    * Revert "A nanoSVG detector workaround for cygwin": Didn't work
    
    This reverts commit ff35e96eb43ef68f4834130ea2f28cfa88de2fc9.
    
    * Another try to repair the nanoSVG logic
    
    * Why did this fail?
    
    * Corrected the logic again
    
    * Compiles again
    
    * Corrected the nanoSVG compilation detector
    
    * If the false-positives go away if we actually use nanoSVG?
    
    * A ugly workaround for nanoSVG
    
    * Yet another nanoSVG instantiation test
    
    * Might compile now

--- src/nanoSVGTest.cpp
+++ src/nanoSVGTest.cpp
@@ -21,37 +21,28 @@
 //  SPDX-License-Identifier: GPL-2.0+
 
 /*! \file
-  This file instantiates nanoSVG.
+  This C++ project instantiates nanoSVG. If it is linked with wxWidgets
+  and wxWidgets provides nanoSVG it fails to build with a linker error.
+  CMake tests for that before deciding if to include nanoSVG.cpp in the
+  wxMaxima sources.
 */
 
-/* The nanosvg .h files contain both the header and the implementation.
-   In exactly one file of the project need to be defined in order to
-   make the implementation visible to the compiler. That might be here -
-   or in wxWidgets.
+/*
+
+  27.8.2023:
+  Local Ubuntu: 
+  Appveyor
 */
 #include <stdio.h>
-#include <wx/wx.h>
-#include "Version.h"
-
-// Before wxWidgets 3.1.6 we need to instantiate nanoSVG.
-// With newer wxWidgets versions than that we needto instantiate it
-// if it doesn't cause a linker error, except on mingw32 where it
-// mysteriously doesn'tl but only in the example cmake tries.
-#if (wxCHECK_VERSION(3, 1, 6))
-#ifdef __MINGW32__
-#else
-#ifdef NANOSVG_CAUSES_NO_LINK_ERROR
-#define INSTANTIATE_NANOSVG 1
-#endif
-#endif
-#else
-#define INSTANTIATE_NANOSVG 1
-#endif
-
-#ifdef INSTANTIATE_NANOSVG
 #define NANOSVG_IMPLEMENTATION
 #define NANOSVGRAST_IMPLEMENTATION
 #define NANOSVG_ALL_COLOR_KEYWORDS
 #include "nanoSVG/nanosvg.h"
 #include "nanoSVG/nanosvgrast.h"
-#endif
+
+int main(int argc, char* argv[]) {
+  struct NSVGimage* image;
+  image = nsvgParseFromFile("test.svg", "px", 96);
+  return 0;
+}
+
