c4dcb796a32426f80183b6f1a71e8a0a412660ce
Gunter Königsmann <gunter@peterpall.de>
2023-11-12 12:47:09 +0100

    A try to make the SVG more nanoSVG compatible

--- src/cells/IntCell.cpp
+++ src/cells/IntCell.cpp
@@ -144,21 +144,8 @@ void IntCell::Draw(wxPoint point, wxDC *dc, wxDC *antialiassingDC) {
 #if wxCHECK_VERSION(3, 1, 6)
         // From: https://commons.wikimedia.org/wiki/File:Integral_Sign.svg (public domain)
         const char* integralSVG = R"svg(
-<?xml version="1.0" standalone="no"?>
-<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN"
- "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd">
-<svg version="1.0" xmlns="http://www.w3.org/2000/svg"
- width="52.000000pt" height="75.000000pt" viewBox="0 0 52.000000 75.000000"
- preserveAspectRatio="xMidYMid meet">
-
-<g transform="translate(0.000000,75.000000) scale(0.100000,-0.100000)"
-fill="#000000" stroke="none">
-<path d="M342 648 c-24 -29 -49 -130 -87 -343 -19 -107 -33 -155 -46 -155 -5
-0 -8 3 -7 7 2 5 0 9 -4 11 -5 1 -8 0 -8 -3 0 -3 0 -9 0 -15 0 -5 9 -10 20 -10
-32 0 48 47 90 262 40 203 59 271 67 236 6 -23 26 -23 21 -1 -4 22 -31 28 -46
-11z"/>
-</g>
-</svg>
+<?xml version="1.0" encoding="UTF-8"?>
+<svg width="19.879pt" height="51.781pt" preserveAspectRatio="xMidYMid" version="1.0" viewBox="0 0 19.879 51.781" xmlns="http://www.w3.org/2000/svg"><g transform="matrix(.1 0 0 -.1 -19 65.781)"><path d="m342 648c-24-29-49-130-87-343-19-107-33-155-46-155-5 0-8 3-7 7 2 5 0 9-4 11-5 1-8 0-8-3v-15c0-5 9-10 20-10 32 0 48 47 90 262 40 203 59 271 67 236 6-23 26-23 21-1-4 22-31 28-46 11z"/></g></svg>
 )svg";
 
         sign.y -= .5 * m_signHeight;
