b8ff04fc5b688fbc8f1db953783dbdc8bf7115bd
Gunter Königsmann <gunter@peterpall.de>
2024-04-29 21:37:17 +0200

    Corrected the framework for #1909

--- src/cells/Cell.cpp
+++ src/cells/Cell.cpp
@@ -137,7 +137,7 @@ wxBitmap Cell::BitmapFromSVG(wxString svgData, wxSize size)
   svgData.Replace("\"currentColor\"",
                   "\"#" + wxColor2HtmlString(GetForegroundColor()) + "\"");
   svgData.Replace("\"currentBackground\"",
-                  "\"#" + wxColor2HtmlString(GetBackgroundColor()) + "\"");
+                  "\"#" + wxColor2HtmlString(m_configuration->DefaultBackgroundColor()) + "\"");
 #if wxCHECK_VERSION(3, 1, 6)
   wxBitmapBundle sumbitmap = wxBitmapBundle::FromSVG(svgData.c_str(),
                                                      size);
