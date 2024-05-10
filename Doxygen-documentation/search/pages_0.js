3e1ba8cd8878ad7d5fe7218f9ccc7231ef6cc5d7
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2024-05-10 20:51:56 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@25b2349e031424dc28eafa7adfddc63114cf1a1f 🚀

--- Doxygen-documentation/search/pages_0.js
+++ Doxygen-documentation/search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['24_2e05_2e0_3393',['24.05.0',['../md_NEWS.html',1,'']]]
+  ['building_20wxmaxima_20from_20source_3393',['Building wxMaxima from source',['../md_Compiling.html',1,'']]]
 ];
