cb6fcebab17de1c4c0ce63a76abb0459d8938d42
daute <github-mail@wolfgang.dautermann.at>
2026-03-08 21:25:41 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@4a946cb9731eb16184f0b5024f83cab1f2ce2721 🚀

--- Doxygen-documentation/search/files_13.js
+++ Doxygen-documentation/search/files_13.js
@@ -7,8 +7,10 @@ var searchData=
   ['wxmathml_2eh_4',['wxMathml.h',['../wxMathml_8h.html',1,'']]],
   ['wxmaxima_2ecpp_5',['wxMaxima.cpp',['../wxMaxima_8cpp.html',1,'']]],
   ['wxmaxima_2eh_6',['wxMaxima.h',['../wxMaxima_8h.html',1,'']]],
-  ['wxmaximaframe_2ecpp_7',['wxMaximaFrame.cpp',['../wxMaximaFrame_8cpp.html',1,'']]],
-  ['wxmaximaframe_2eh_8',['wxMaximaFrame.h',['../wxMaximaFrame_8h.html',1,'']]],
-  ['wxmaximaicon_2ecpp_9',['wxMaximaIcon.cpp',['../wxMaximaIcon_8cpp.html',1,'']]],
-  ['wxmaximaicon_2eh_10',['wxMaximaIcon.h',['../wxMaximaIcon_8h.html',1,'']]]
+  ['wxmaximaartprovider_2ecpp_7',['wxMaximaArtProvider.cpp',['../wxMaximaArtProvider_8cpp.html',1,'']]],
+  ['wxmaximaartprovider_2eh_8',['wxMaximaArtProvider.h',['../wxMaximaArtProvider_8h.html',1,'']]],
+  ['wxmaximaframe_2ecpp_9',['wxMaximaFrame.cpp',['../wxMaximaFrame_8cpp.html',1,'']]],
+  ['wxmaximaframe_2eh_10',['wxMaximaFrame.h',['../wxMaximaFrame_8h.html',1,'']]],
+  ['wxmaximaicon_2ecpp_11',['wxMaximaIcon.cpp',['../wxMaximaIcon_8cpp.html',1,'']]],
+  ['wxmaximaicon_2eh_12',['wxMaximaIcon.h',['../wxMaximaIcon_8h.html',1,'']]]
 ];
