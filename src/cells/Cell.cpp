2429b2c1ef3c916f06084904c9fe602a990352c6
Gunter Königsmann <gunter@peterpall.de>
2026-05-18 09:40:07 +0200

    More work on build warnings

--- src/cells/Cell.cpp
+++ src/cells/Cell.cpp
@@ -140,10 +140,6 @@ wxBitmap Cell::BitmapFromSVG(wxString svgData, wxSize size)
 #if wxCHECK_VERSION(3, 1, 6)
   wxBitmapBundle sumbitmap = wxBitmapBundle::FromSVG(svgData.c_str(),
                                                      size);
-  // Make the bitmap hi-res, if the OS supports and needs that
-  const wxWindow *worksheet = m_configuration->GetWorkSheet();
-  if(worksheet)
-    sumbitmap.GetPreferredBitmapSizeFor(worksheet);
   wxBitmap bmp(sumbitmap.GetBitmap(size));
 #else
   SvgBitmap bmp(m_configuration->GetWorkSheet(),
