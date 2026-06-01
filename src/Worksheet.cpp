wxMaxima-developers/wxmaxima

commit ff3e1b800d7a96484d5350c7ed32e370264738ad

GroupCells now grow again if their TextCell changes size.

src/Worksheet.cpp
@@ -3490,6 +3490,7 @@ void Worksheet::OnCharInActive(wxKeyEvent &event) {
   ///
   const EditorCell *activeCell = GetActiveCell();
   wxString oldValue = activeCell->GetValue();
+  int oldHeight = activeCell->GetHeight();
 
   switch (event.GetKeyCode()) {
   case WXK_LEFT:
@@ -3536,13 +3537,12 @@ void Worksheet::OnCharInActive(wxKeyEvent &event) {
   if (activeCell->IsDirty()) {
     SetSaved(false);
 
-    int height = activeCell->GetHeight();
     //   int fontsize = m_configuration->GetDefaultFontSize();
     auto fontsize = m_configuration->GetDefaultFontSize();
 
     GetActiveCell()->Recalculate(std::max(fontsize, MC_MIN_SIZE));
 
-    if (height != GetActiveCell()->GetHeight() ||
+    if (oldHeight != GetActiveCell()->GetHeight() ||
         GetActiveCell()->GetWidth() + GetActiveCell()->GetCurrentPoint().x >=
         GetClientSize().GetWidth() -
         m_configuration->GetCellBracketWidth() -

src/cells/Cell.cpp
@@ -1223,11 +1223,6 @@ void Cell::ResetSize() const {
 
 void Cell::InvalidateListCache() const {
   for (const Cell *walk = this; walk; walk = walk->m_previous) {
-    if (walk->m_cachedMaxDrop.IsInvalid() &&
-        walk->m_cachedCenterList.IsInvalid() &&
-        walk->m_cachedSumOfWidths.IsInvalid() &&
-        walk->m_cachedLineWidth.IsInvalid())
-      break;
     walk->m_cachedMaxDrop.Invalidate();
     walk->m_cachedCenterList.Invalidate();
     walk->m_cachedSumOfWidths.Invalidate();

src/cells/GroupCell.cpp
@@ -643,7 +643,7 @@ void GroupCell::UpdateOutputPositions() const {
     wxPoint in = m_currentPoint;
     // Offset results below the input area
     if (m_configuration->ShowCodeCells() || (m_groupType != GC_TYPE_CODE))
-      in.y += m_inputLabel->GetMaxDrop();
+      in.y += (m_inputHeight - m_center);
 
     // The outputRect defines the bounding box for all results
     const_cast<GroupCell *>(this)->m_outputRect.SetPosition(in);

commit 97d908581a095e0435265a573d30369180bf2bbe

Commit ff3e1b800d7a96484d5350c7ed32e370264738ad caused wxMaxima to get too slow

src/Worksheet.cpp
@@ -3552,7 +3552,7 @@ void Worksheet::OnCharInActive(wxKeyEvent &event) {
   /// If we need to recalculate then refresh the window
   if (needRecalculate) {
     GroupCell *group = GetActiveCell()->GetGroup();
-    group->ResetSize_Recursively();
+    group->ResetSize_Recursively_Force();
     if (GetActiveCell()->CheckChanges() &&
         (group->GetGroupType() == GC_TYPE_CODE) &&
         (GetActiveCell() == group->GetEditable()))

src/cells/Cell.cpp
@@ -1206,6 +1206,7 @@ void Cell::ResetSize_Recursively() const {
       tmp.ResetSize_Recursively();
 }
 
+
 void Cell::ResetSize_RecursivelyList() const {
   for (const Cell &tmp : OnList(this))
     tmp.ResetSize_Recursively();
@@ -1223,6 +1224,12 @@ void Cell::ResetSize() const {
 
 void Cell::InvalidateListCache() const {
   for (const Cell *walk = this; walk; walk = walk->m_previous) {
+    if (walk->m_cachedMaxDrop.IsInvalid() &&
+        walk->m_cachedCenterList.IsInvalid() &&
+        walk->m_cachedSumOfWidths.IsInvalid() &&
+        walk->m_cachedLineWidth.IsInvalid())
+      break;
+    
     walk->m_cachedMaxDrop.Invalidate();
     walk->m_cachedCenterList.Invalidate();
     walk->m_cachedSumOfWidths.Invalidate();