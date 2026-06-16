6217c8f17e0626a9f3a81b435c4ab422ccc97894
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-16 10:03:21 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@6bf32534dc13ae9a639cb03a8c8ce1b415d26219 🚀

--- Doxygen-documentation/search/pages_8.js
+++ Doxygen-documentation/search/pages_8.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['oss_2dfuzz_20integration_20files_0',['OSS-Fuzz integration files',['../md_test_fuzz_oss_fuzz_README.html',1,'']]]
+  ['pag_5fexample_0',['pag_example',['../pag_example.html',1,'']]],
+  ['parser_20fuzzers_1',['Parser fuzzers',['../md_test_fuzz_README.html',1,'']]],
+  ['project_20instructions_2',['Project Instructions',['../md_AGENTS.html',1,'']]]
 ];
--- Doxygen-documentation/search/pages_9.js
+++ Doxygen-documentation/search/pages_9.js
@@ -1,6 +1,4 @@
 var searchData=
 [
-  ['pag_5fexample_0',['pag_example',['../pag_example.html',1,'']]],
-  ['parser_20fuzzers_1',['Parser fuzzers',['../md_test_fuzz_README.html',1,'']]],
-  ['project_20instructions_2',['Project Instructions',['../md_AGENTS.html',1,'']]]
+  ['readme_0',['README',['../md_art_README.html',1,'(Global Namespace)'],['../md_cmake_bin2h_README.html',1,'(Global Namespace)']]]
 ];
