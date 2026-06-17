a4957e01178c48dcee901c07bfc6373848fa4671
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-17 19:09:59 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@7b7cd0b9543dad043cd7cb9d83c4264a1804cbd0 🚀

--- Doxygen-documentation/search/files_10.js
+++ Doxygen-documentation/search/files_10.js
@@ -2,9 +2,10 @@ var searchData=
 [
   ['tableofcontents_2ecpp_0',['TableOfContents.cpp',['../TableOfContents_8cpp.html',1,'']]],
   ['tableofcontents_2eh_1',['TableOfContents.h',['../TableOfContents_8h.html',1,'']]],
-  ['textcell_2ecpp_2',['TextCell.cpp',['../TextCell_8cpp.html',1,'']]],
-  ['textstyle_2ecpp_3',['TextStyle.cpp',['../TextStyle_8cpp.html',1,'']]],
-  ['textstyle_2eh_4',['TextStyle.h',['../TextStyle_8h.html',1,'']]],
-  ['toolbar_2ecpp_5',['ToolBar.cpp',['../ToolBar_8cpp.html',1,'']]],
-  ['toolbar_2eh_6',['ToolBar.h',['../ToolBar_8h.html',1,'']]]
+  ['test_5fgroupcelllayout_2ecpp_2',['test_GroupCellLayout.cpp',['../test__GroupCellLayout_8cpp.html',1,'']]],
+  ['textcell_2ecpp_3',['TextCell.cpp',['../TextCell_8cpp.html',1,'']]],
+  ['textstyle_2ecpp_4',['TextStyle.cpp',['../TextStyle_8cpp.html',1,'']]],
+  ['textstyle_2eh_5',['TextStyle.h',['../TextStyle_8h.html',1,'']]],
+  ['toolbar_2ecpp_6',['ToolBar.cpp',['../ToolBar_8cpp.html',1,'']]],
+  ['toolbar_2eh_7',['ToolBar.h',['../ToolBar_8h.html',1,'']]]
 ];
