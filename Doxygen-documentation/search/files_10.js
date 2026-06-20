d7a12ddc5c7299d3a751933ac920112da507d0f9
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-20 16:27:35 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@da3c0ccff7962d06dcef0810fd2f224c94882bed 🚀

--- Doxygen-documentation/search/files_10.js
+++ Doxygen-documentation/search/files_10.js
@@ -3,10 +3,11 @@ var searchData=
   ['tableofcontents_2ecpp_0',['TableOfContents.cpp',['../TableOfContents_8cpp.html',1,'']]],
   ['tableofcontents_2eh_1',['TableOfContents.h',['../TableOfContents_8h.html',1,'']]],
   ['test_5fdiffscrollsync_2ecpp_2',['test_DiffScrollSync.cpp',['../test__DiffScrollSync_8cpp.html',1,'']]],
-  ['test_5fgroupcelllayout_2ecpp_3',['test_GroupCellLayout.cpp',['../test__GroupCellLayout_8cpp.html',1,'']]],
-  ['textcell_2ecpp_4',['TextCell.cpp',['../TextCell_8cpp.html',1,'']]],
-  ['textstyle_2ecpp_5',['TextStyle.cpp',['../TextStyle_8cpp.html',1,'']]],
-  ['textstyle_2eh_6',['TextStyle.h',['../TextStyle_8h.html',1,'']]],
-  ['toolbar_2ecpp_7',['ToolBar.cpp',['../ToolBar_8cpp.html',1,'']]],
-  ['toolbar_2eh_8',['ToolBar.h',['../ToolBar_8h.html',1,'']]]
+  ['test_5feditorcellinvariants_2ecpp_3',['test_EditorCellInvariants.cpp',['../test__EditorCellInvariants_8cpp.html',1,'']]],
+  ['test_5fgroupcelllayout_2ecpp_4',['test_GroupCellLayout.cpp',['../test__GroupCellLayout_8cpp.html',1,'']]],
+  ['textcell_2ecpp_5',['TextCell.cpp',['../TextCell_8cpp.html',1,'']]],
+  ['textstyle_2ecpp_6',['TextStyle.cpp',['../TextStyle_8cpp.html',1,'']]],
+  ['textstyle_2eh_7',['TextStyle.h',['../TextStyle_8h.html',1,'']]],
+  ['toolbar_2ecpp_8',['ToolBar.cpp',['../ToolBar_8cpp.html',1,'']]],
+  ['toolbar_2eh_9',['ToolBar.h',['../ToolBar_8h.html',1,'']]]
 ];
