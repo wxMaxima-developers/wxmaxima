9960f3be05eac5f0ed8108b97ebda9294bb57a80
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-20 06:27:36 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@c0c7c37b38053280ba5b48509ba4914ba2a40d4b 🚀

--- Doxygen-documentation/search/files_3.js
+++ Doxygen-documentation/search/files_3.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['define_2eh_0',['define.h',['../define_8h.html',1,'']]],
   ['diffcell_2ecpp_1',['DiffCell.cpp',['../DiffCell_8cpp.html',1,'']]],
-  ['digitcell_2ecpp_2',['DigitCell.cpp',['../DigitCell_8cpp.html',1,'']]],
-  ['dirstructure_2ecpp_3',['Dirstructure.cpp',['../Dirstructure_8cpp.html',1,'']]],
-  ['dirstructure_2eh_4',['Dirstructure.h',['../Dirstructure_8h.html',1,'']]],
-  ['drawsidebar_2ecpp_5',['DrawSidebar.cpp',['../DrawSidebar_8cpp.html',1,'']]],
-  ['drawsidebar_2eh_6',['DrawSidebar.h',['../DrawSidebar_8h.html',1,'']]],
-  ['drawwiz_2ecpp_7',['DrawWiz.cpp',['../DrawWiz_8cpp.html',1,'']]],
-  ['drawwiz_2eh_8',['DrawWiz.h',['../DrawWiz_8h.html',1,'']]]
+  ['diffscrollsync_2eh_2',['DiffScrollSync.h',['../DiffScrollSync_8h.html',1,'']]],
+  ['digitcell_2ecpp_3',['DigitCell.cpp',['../DigitCell_8cpp.html',1,'']]],
+  ['dirstructure_2ecpp_4',['Dirstructure.cpp',['../Dirstructure_8cpp.html',1,'']]],
+  ['dirstructure_2eh_5',['Dirstructure.h',['../Dirstructure_8h.html',1,'']]],
+  ['drawsidebar_2ecpp_6',['DrawSidebar.cpp',['../DrawSidebar_8cpp.html',1,'']]],
+  ['drawsidebar_2eh_7',['DrawSidebar.h',['../DrawSidebar_8h.html',1,'']]],
+  ['drawwiz_2ecpp_8',['DrawWiz.cpp',['../DrawWiz_8cpp.html',1,'']]],
+  ['drawwiz_2eh_9',['DrawWiz.h',['../DrawWiz_8h.html',1,'']]]
 ];
--- Doxygen-documentation/search/variables_3.js
+++ Doxygen-documentation/search/variables_3.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['description_0',['description',['../structCharButton_1_1Definition.html#ab873b11925f89f2e13c50e507c4e4319',1,'CharButton::Definition']]],
-  ['din_5f0_1',['din_0',['../classmux__using__with.html#a9fc14b65611c7124d4cab27b963f3011',1,'mux_using_with']]],
-  ['din_5f1_2',['din_1',['../classmux__using__with.html#aaedccad88ca9b207c86a561342fcbaa1',1,'mux_using_with']]]
+  ['diffsync_5fno_5fcell_1',['DIFFSYNC_NO_CELL',['../DiffScrollSync_8h.html#aadc5553c667fa03eb20753894f654105',1,'DiffScrollSync.h']]],
+  ['din_5f0_2',['din_0',['../classmux__using__with.html#a9fc14b65611c7124d4cab27b963f3011',1,'mux_using_with']]],
+  ['din_5f1_3',['din_1',['../classmux__using__with.html#aaedccad88ca9b207c86a561342fcbaa1',1,'mux_using_with']]]
 ];
