d53e071f7f89d12a66ead7f1ee31a449cad03777
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-20 21:48:03 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@1892047477e5c7b138bbf9c9300b988677f7408e 🚀

--- Doxygen-documentation/search/functions_b.js
+++ Doxygen-documentation/search/functions_b.js
@@ -1,7 +1,8 @@
 var searchData=
 [
-  ['keyboardselectionstart_0',['KeyboardSelectionStart',['../classWorksheet.html#aa3a64ca2a00762474da8be283bae84c2',1,'Worksheet']]],
-  ['keyboardselectionstartedhere_1',['KeyboardSelectionStartedHere',['../classEditorCell.html#a657f28462a91ec41213a446b5b85bc31',1,'EditorCell']]],
-  ['killallchildmaximas_2',['KillAllChildMaximas',['../classwxMaxima.html#a1e5c1ed65fc99a2aeffc59acf0d1d456',1,'wxMaxima']]],
-  ['killmaxima_3',['KillMaxima',['../classwxMaxima.html#a8da4fe8ce1571a6e9f1a6cdf7cd8e270',1,'wxMaxima']]]
+  ['keyboardinactivetimer_0',['KeyboardInactiveTimer',['../classWorksheet.html#a79d3f9c91582dac67a19780cd5c4006c',1,'Worksheet']]],
+  ['keyboardselectionstart_1',['KeyboardSelectionStart',['../classWorksheet.html#aa3a64ca2a00762474da8be283bae84c2',1,'Worksheet']]],
+  ['keyboardselectionstartedhere_2',['KeyboardSelectionStartedHere',['../classEditorCell.html#a657f28462a91ec41213a446b5b85bc31',1,'EditorCell']]],
+  ['killallchildmaximas_3',['KillAllChildMaximas',['../classwxMaxima.html#a1e5c1ed65fc99a2aeffc59acf0d1d456',1,'wxMaxima']]],
+  ['killmaxima_4',['KillMaxima',['../classwxMaxima.html#a8da4fe8ce1571a6e9f1a6cdf7cd8e270',1,'wxMaxima']]]
 ];
