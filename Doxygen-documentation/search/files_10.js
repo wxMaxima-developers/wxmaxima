4ad98db1c2dcc1ff6fea3090cdbd634d2f944509
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-20 06:57:07 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@bf966c04e057cf9bc8fb2ddf4d3ca9532ca39cc2 🚀

--- Doxygen-documentation/search/files_10.js
+++ Doxygen-documentation/search/files_10.js
@@ -2,10 +2,11 @@ var searchData=
 [
   ['tableofcontents_2ecpp_0',['TableOfContents.cpp',['../TableOfContents_8cpp.html',1,'']]],
   ['tableofcontents_2eh_1',['TableOfContents.h',['../TableOfContents_8h.html',1,'']]],
-  ['test_5fgroupcelllayout_2ecpp_2',['test_GroupCellLayout.cpp',['../test__GroupCellLayout_8cpp.html',1,'']]],
-  ['textcell_2ecpp_3',['TextCell.cpp',['../TextCell_8cpp.html',1,'']]],
-  ['textstyle_2ecpp_4',['TextStyle.cpp',['../TextStyle_8cpp.html',1,'']]],
-  ['textstyle_2eh_5',['TextStyle.h',['../TextStyle_8h.html',1,'']]],
-  ['toolbar_2ecpp_6',['ToolBar.cpp',['../ToolBar_8cpp.html',1,'']]],
-  ['toolbar_2eh_7',['ToolBar.h',['../ToolBar_8h.html',1,'']]]
+  ['test_5fdiffscrollsync_2ecpp_2',['test_DiffScrollSync.cpp',['../test__DiffScrollSync_8cpp.html',1,'']]],
+  ['test_5fgroupcelllayout_2ecpp_3',['test_GroupCellLayout.cpp',['../test__GroupCellLayout_8cpp.html',1,'']]],
+  ['textcell_2ecpp_4',['TextCell.cpp',['../TextCell_8cpp.html',1,'']]],
+  ['textstyle_2ecpp_5',['TextStyle.cpp',['../TextStyle_8cpp.html',1,'']]],
+  ['textstyle_2eh_6',['TextStyle.h',['../TextStyle_8h.html',1,'']]],
+  ['toolbar_2ecpp_7',['ToolBar.cpp',['../ToolBar_8cpp.html',1,'']]],
+  ['toolbar_2eh_8',['ToolBar.h',['../ToolBar_8h.html',1,'']]]
 ];
