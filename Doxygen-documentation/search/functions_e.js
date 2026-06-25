01586beefcf4fc0ed9289c9a0af452a522f628b1
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-06-25 03:00:50 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@3417e77a6e1faadf23a9898bc730170e47ab0467 🚀

--- Doxygen-documentation/search/functions_e.js
+++ Doxygen-documentation/search/functions_e.js
@@ -1,14 +1,15 @@
 var searchData=
 [
-  ['namedboxcell_0',['NamedBoxCell',['../classNamedBoxCell.html#a8a21868ef0a9301271bdaad09eef6705',1,'NamedBoxCell']]],
-  ['needsrecalculation_1',['NeedsRecalculation',['../classCell.html#aa0cc4910d52a26419062952d95d294bf',1,'Cell::NeedsRecalculation(AFontSize fontSize) const'],['../classCell.html#af02e5376702194e709fb2d2b493364fc',1,'Cell::NeedsRecalculation() const'],['../classEditorCell.html#a27361d11dfbf4383dcdfa11cb227e5ed',1,'EditorCell::NeedsRecalculation()'],['../classGroupCell.html#a2b1a8fb21cbb4f11e3a4c17726645a44',1,'GroupCell::NeedsRecalculation(AFontSize fontSize) const override'],['../classGroupCell.html#aa0cc4910d52a26419062952d95d294bf',1,'GroupCell::NeedsRecalculation(AFontSize fontSize) const'],['../classGroupCell.html#af02e5376702194e709fb2d2b493364fc',1,'GroupCell::NeedsRecalculation() const']]],
-  ['networkdclick_2',['NetworkDClick',['../classwxMaxima.html#a2fd4dac2b28c1c2aff8609f451dedabd',1,'wxMaxima']]],
-  ['networkstatus_3',['NetworkStatus',['../classStatusBar.html#addf51766d62198da4dd162f7f2a05c6f',1,'StatusBar']]],
-  ['newwizard_4',['NewWizard',['../classGenWizPanel.html#abe9a1ef102fb3f9de0cf84a20562eda1',1,'GenWizPanel']]],
-  ['notification_5',['Notification',['../classNotification.html#a1d2014b23265060cebccfb542a60e75a',1,'Notification::Notification()'],['../classNotification.html#a318b399339a243424905d071abc553c8',1,'Notification::Notification(const wxString &amp;title, const wxString &amp;message={}, wxWindow *parent=NULL, int flags=wxICON_INFORMATION)']]],
-  ['notifyifidle_6',['NotifyIfIdle',['../classConfiguration.html#a35914468c50b5421c93c802f72b5ce9e',1,'Configuration']]],
-  ['notifyofcellredraw_7',['NotifyOfCellRedraw',['../classConfiguration.html#ad47f83d888f67fc6e303b5c0f6faa7ca',1,'Configuration']]],
-  ['number_8',['Number',['../classGroupCell.html#a1bc7ca9e1bf5923dbdd5e2e8735da8cd',1,'GroupCell']]],
-  ['numbersections_9',['NumberSections',['../classWorksheet.html#a9770595891b32c0ea91ea4d4466c9310',1,'Worksheet']]],
-  ['numericalmenu_10',['NumericalMenu',['../classwxMaxima.html#a9c1ad69ebd56bb77c7f403d967f2ced2',1,'wxMaxima']]]
+  ['name_0',['Name',['../classStyles.html#a21bf4943137ee7b3ce6971d9eecc51ea',1,'Styles']]],
+  ['namedboxcell_1',['NamedBoxCell',['../classNamedBoxCell.html#a8a21868ef0a9301271bdaad09eef6705',1,'NamedBoxCell']]],
+  ['needsrecalculation_2',['NeedsRecalculation',['../classCell.html#aa0cc4910d52a26419062952d95d294bf',1,'Cell::NeedsRecalculation(AFontSize fontSize) const'],['../classCell.html#af02e5376702194e709fb2d2b493364fc',1,'Cell::NeedsRecalculation() const'],['../classEditorCell.html#a27361d11dfbf4383dcdfa11cb227e5ed',1,'EditorCell::NeedsRecalculation()'],['../classGroupCell.html#a2b1a8fb21cbb4f11e3a4c17726645a44',1,'GroupCell::NeedsRecalculation(AFontSize fontSize) const override'],['../classGroupCell.html#aa0cc4910d52a26419062952d95d294bf',1,'GroupCell::NeedsRecalculation(AFontSize fontSize) const'],['../classGroupCell.html#af02e5376702194e709fb2d2b493364fc',1,'GroupCell::NeedsRecalculation() const']]],
+  ['networkdclick_3',['NetworkDClick',['../classwxMaxima.html#a2fd4dac2b28c1c2aff8609f451dedabd',1,'wxMaxima']]],
+  ['networkstatus_4',['NetworkStatus',['../classStatusBar.html#addf51766d62198da4dd162f7f2a05c6f',1,'StatusBar']]],
+  ['newwizard_5',['NewWizard',['../classGenWizPanel.html#abe9a1ef102fb3f9de0cf84a20562eda1',1,'GenWizPanel']]],
+  ['notification_6',['Notification',['../classNotification.html#a1d2014b23265060cebccfb542a60e75a',1,'Notification::Notification()'],['../classNotification.html#a318b399339a243424905d071abc553c8',1,'Notification::Notification(const wxString &amp;title, const wxString &amp;message={}, wxWindow *parent=NULL, int flags=wxICON_INFORMATION)']]],
+  ['notifyifidle_7',['NotifyIfIdle',['../classConfiguration.html#a35914468c50b5421c93c802f72b5ce9e',1,'Configuration']]],
+  ['notifyofcellredraw_8',['NotifyOfCellRedraw',['../classConfiguration.html#ad47f83d888f67fc6e303b5c0f6faa7ca',1,'Configuration']]],
+  ['number_9',['Number',['../classGroupCell.html#a1bc7ca9e1bf5923dbdd5e2e8735da8cd',1,'GroupCell']]],
+  ['numbersections_10',['NumberSections',['../classWorksheet.html#a9770595891b32c0ea91ea4d4466c9310',1,'Worksheet']]],
+  ['numericalmenu_11',['NumericalMenu',['../classwxMaxima.html#a9c1ad69ebd56bb77c7f403d967f2ced2',1,'wxMaxima']]]
 ];
