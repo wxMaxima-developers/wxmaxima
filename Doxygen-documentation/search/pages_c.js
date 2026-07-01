09cdef137ec7abf6aa2675ebed81ab353f2c21fc
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-01 10:41:04 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@aeb5149b4902d2099d11f60857af8bfac8e32614 🚀

--- Doxygen-documentation/search/pages_c.js
+++ Doxygen-documentation/search/pages_c.js
@@ -2,6 +2,7 @@ var searchData=
 [
   ['wxmaxima_0',['wxMaxima',['../index.html',1,'']]],
   ['wxmaxima_1',['WxMaxima',['../md_README.html',1,'']]],
-  ['wxmaxima_20release_20checklist_2',['wxMaxima Release Checklist',['../md_ReleaseChecklist.html',1,'']]],
-  ['wxmaxima_27s_20translations_3',['wxMaxima&apos;s Translations',['../md_locales_README.html',1,'']]]
+  ['wxmaxima_20accessibility_20audit_2',['wxMaxima accessibility audit',['../md_ACCESSIBILITY.html',1,'']]],
+  ['wxmaxima_20release_20checklist_3',['wxMaxima Release Checklist',['../md_ReleaseChecklist.html',1,'']]],
+  ['wxmaxima_27s_20translations_4',['wxMaxima&apos;s Translations',['../md_locales_README.html',1,'']]]
 ];
