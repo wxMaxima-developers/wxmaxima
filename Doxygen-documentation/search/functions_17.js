d39c162f7ede07cf2b71774fd227144939236fe2
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-10 11:57:03 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@55800e4261a3eb31e2681fca97fc40de7d2ea42e 🚀

--- Doxygen-documentation/search/functions_17.js
+++ Doxygen-documentation/search/functions_17.js
@@ -5,11 +5,12 @@ var searchData=
   ['wizdrawaccuracy_2',['WizDrawAccuracy',['../classWizDrawAccuracy.html#a14a027a1623d28baa6d64e74f4d67e61',1,'WizDrawAccuracy']]],
   ['workers_3',['Workers',['../classBackgroundQueue.html#a31458e8c027f4f5ff1452ecb89804401',1,'BackgroundQueue']]],
   ['worksheet_4',['Worksheet',['../classWorksheet.html#a7ba34e68818f5c6b3374773683e5710c',1,'Worksheet']]],
-  ['write_5',['Write',['../classStyle.html#a259bf27abcb1f6d66dbb02bc9bc9f6cb',1,'Style::Write()'],['../classMaxima.html#aeb45e2fda6a941f99aa9181b644c75be',1,'Maxima::Write()'],['../classStyles.html#aaea29fa13c11dede7a91739e1da4f4ce',1,'Styles::Write()']]],
-  ['write_6',['write',['../structcmd_8h.html#af2a3ea719b83f672637febdd87c36c36',1,'structcmd.h']]],
-  ['writesettings_7',['WriteSettings',['../classConfiguration.html#a53af46fba084e7e9f45fabbfb8eb4030',1,'Configuration::WriteSettings()'],['../classConfigDialogue.html#a6a3d11ed034adf263350d8aa54a7e28d',1,'ConfigDialogue::WriteSettings()']]],
-  ['writestyles_8',['WriteStyles',['../classConfiguration.html#a4f7f692a8178c2062765ddafd0fd4782',1,'Configuration']]],
-  ['writeto_9',['WriteTo',['../classFontRenderabilityCache.html#abd83076fd77f72751a213f6c4ae84077',1,'FontRenderabilityCache']]],
-  ['wxmaximaicon_10',['wxMaximaIcon',['../wxMaximaIcon_8cpp.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp'],['../wxMaximaIcon_8h.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp']]],
-  ['wxmaximaoperatingsystemdescription_11',['wxMaximaOperatingSystemDescription',['../wxMaximaOSDescription_8cpp.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp'],['../wxMaximaOSDescription_8h.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp']]]
+  ['worksheetlayout_5',['WorksheetLayout',['../classWorksheetLayout.html#a53fa578d33254cb3c4ad0f27f66ecbdc',1,'WorksheetLayout']]],
+  ['write_6',['Write',['../classStyle.html#a259bf27abcb1f6d66dbb02bc9bc9f6cb',1,'Style::Write()'],['../classMaxima.html#aeb45e2fda6a941f99aa9181b644c75be',1,'Maxima::Write()'],['../classStyles.html#aaea29fa13c11dede7a91739e1da4f4ce',1,'Styles::Write()']]],
+  ['write_7',['write',['../structcmd_8h.html#af2a3ea719b83f672637febdd87c36c36',1,'structcmd.h']]],
+  ['writesettings_8',['WriteSettings',['../classConfiguration.html#a53af46fba084e7e9f45fabbfb8eb4030',1,'Configuration::WriteSettings()'],['../classConfigDialogue.html#a6a3d11ed034adf263350d8aa54a7e28d',1,'ConfigDialogue::WriteSettings()']]],
+  ['writestyles_9',['WriteStyles',['../classConfiguration.html#a4f7f692a8178c2062765ddafd0fd4782',1,'Configuration']]],
+  ['writeto_10',['WriteTo',['../classFontRenderabilityCache.html#abd83076fd77f72751a213f6c4ae84077',1,'FontRenderabilityCache']]],
+  ['wxmaximaicon_11',['wxMaximaIcon',['../wxMaximaIcon_8cpp.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp'],['../wxMaximaIcon_8h.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp']]],
+  ['wxmaximaoperatingsystemdescription_12',['wxMaximaOperatingSystemDescription',['../wxMaximaOSDescription_8cpp.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp'],['../wxMaximaOSDescription_8h.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp']]]
 ];
