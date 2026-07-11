ee63fd45f9a19e5fb83d1eef2d10eae0eb81d0f2
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-11 04:52:12 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@e7c4e034c92398ff4d5f729525511a89190d302c 🚀

--- Doxygen-documentation/search/functions_11.js
+++ Doxygen-documentation/search/functions_11.js
@@ -3,5 +3,5 @@ var searchData=
   ['qtstyle_5ftest_0',['QTstyle_Test',['../classQTstyle__Test.html#a14a296ea4e2ad446712f2310bec60766',1,'QTstyle_Test']]],
   ['queryvariablevalue_1',['QueryVariableValue',['../classwxMaxima.html#acee1bc4f6279f20c43e8063a1ea1ccd6',1,'wxMaxima']]],
   ['questionanswered_2',['QuestionAnswered',['../classWorksheet.html#ae03cf21f8f8398e2591cb61f50614ffd',1,'Worksheet']]],
-  ['questionpending_3',['QuestionPending',['../classWorksheet.html#a213f82e67d6b8602c55d0e509d59976f',1,'Worksheet::QuestionPending() const'],['../classWorksheet.html#a3d30157e25a0e4ba2bf780de877fed10',1,'Worksheet::QuestionPending(bool pending)']]]
+  ['questionpending_3',['QuestionPending',['../classWorksheet.html#a213f82e67d6b8602c55d0e509d59976f',1,'Worksheet::QuestionPending() const'],['../classWorksheet.html#a3d30157e25a0e4ba2bf780de877fed10',1,'Worksheet::QuestionPending(bool pending)'],['../classWorksheetDocument.html#a357c554273f4babcfe789d605749d1d4',1,'WorksheetDocument::QuestionPending()']]]
 ];
