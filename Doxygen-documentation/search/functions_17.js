3fc963f539ea134f3ab4c432f5575dbee6d951ac
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-13 12:57:56 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@7468cc8aa4031f16572497e338af3f552fcf37fa 🚀

--- Doxygen-documentation/search/functions_17.js
+++ Doxygen-documentation/search/functions_17.js
@@ -1,16 +1,18 @@
 var searchData=
 [
   ['wait_0',['Wait',['../classBackgroundTask.html#a9a3abbef089d6ab6eb5f546240554cbb',1,'BackgroundTask']]],
-  ['windowactive_1',['WindowActive',['../classWorksheet.html#aae739739bfc9ac5c25242ea303d62dcd',1,'Worksheet']]],
-  ['wizdrawaccuracy_2',['WizDrawAccuracy',['../classWizDrawAccuracy.html#a14a027a1623d28baa6d64e74f4d67e61',1,'WizDrawAccuracy']]],
-  ['workers_3',['Workers',['../classBackgroundQueue.html#a31458e8c027f4f5ff1452ecb89804401',1,'BackgroundQueue']]],
-  ['worksheet_4',['Worksheet',['../classWorksheet.html#a7ba34e68818f5c6b3374773683e5710c',1,'Worksheet']]],
-  ['worksheetlayout_5',['WorksheetLayout',['../classWorksheetLayout.html#a53fa578d33254cb3c4ad0f27f66ecbdc',1,'WorksheetLayout']]],
-  ['write_6',['Write',['../classStyle.html#a259bf27abcb1f6d66dbb02bc9bc9f6cb',1,'Style::Write()'],['../classMaxima.html#aeb45e2fda6a941f99aa9181b644c75be',1,'Maxima::Write()'],['../classStyles.html#aaea29fa13c11dede7a91739e1da4f4ce',1,'Styles::Write()']]],
-  ['write_7',['write',['../structcmd_8h.html#af2a3ea719b83f672637febdd87c36c36',1,'structcmd.h']]],
-  ['writesettings_8',['WriteSettings',['../classConfiguration.html#a53af46fba084e7e9f45fabbfb8eb4030',1,'Configuration::WriteSettings()'],['../classConfigDialogue.html#a6a3d11ed034adf263350d8aa54a7e28d',1,'ConfigDialogue::WriteSettings()']]],
-  ['writestyles_9',['WriteStyles',['../classConfiguration.html#a4f7f692a8178c2062765ddafd0fd4782',1,'Configuration']]],
-  ['writeto_10',['WriteTo',['../classFontRenderabilityCache.html#abd83076fd77f72751a213f6c4ae84077',1,'FontRenderabilityCache']]],
-  ['wxmaximaicon_11',['wxMaximaIcon',['../wxMaximaIcon_8cpp.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp'],['../wxMaximaIcon_8h.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp']]],
-  ['wxmaximaoperatingsystemdescription_12',['wxMaximaOperatingSystemDescription',['../wxMaximaOSDescription_8cpp.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp'],['../wxMaximaOSDescription_8h.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp']]]
+  ['willautoanswer_1',['WillAutoAnswer',['../classWorksheet.html#a94bd94ddb17fece8123859edd328662c',1,'Worksheet']]],
+  ['windowactive_2',['WindowActive',['../classWorksheet.html#aae739739bfc9ac5c25242ea303d62dcd',1,'Worksheet']]],
+  ['wizdrawaccuracy_3',['WizDrawAccuracy',['../classWizDrawAccuracy.html#a14a027a1623d28baa6d64e74f4d67e61',1,'WizDrawAccuracy']]],
+  ['workers_4',['Workers',['../classBackgroundQueue.html#a31458e8c027f4f5ff1452ecb89804401',1,'BackgroundQueue']]],
+  ['worksheet_5',['Worksheet',['../classWorksheet.html#a7ba34e68818f5c6b3374773683e5710c',1,'Worksheet']]],
+  ['worksheetlayout_6',['WorksheetLayout',['../classWorksheetLayout.html#a53fa578d33254cb3c4ad0f27f66ecbdc',1,'WorksheetLayout']]],
+  ['write_7',['Write',['../classStyles.html#aaea29fa13c11dede7a91739e1da4f4ce',1,'Styles']]],
+  ['write_8',['write',['../structcmd_8h.html#af2a3ea719b83f672637febdd87c36c36',1,'structcmd.h']]],
+  ['write_9',['Write',['../classMaxima.html#aeb45e2fda6a941f99aa9181b644c75be',1,'Maxima::Write()'],['../classStyle.html#a259bf27abcb1f6d66dbb02bc9bc9f6cb',1,'Style::Write()']]],
+  ['writesettings_10',['WriteSettings',['../classConfiguration.html#a53af46fba084e7e9f45fabbfb8eb4030',1,'Configuration::WriteSettings()'],['../classConfigDialogue.html#a6a3d11ed034adf263350d8aa54a7e28d',1,'ConfigDialogue::WriteSettings()']]],
+  ['writestyles_11',['WriteStyles',['../classConfiguration.html#a4f7f692a8178c2062765ddafd0fd4782',1,'Configuration']]],
+  ['writeto_12',['WriteTo',['../classFontRenderabilityCache.html#abd83076fd77f72751a213f6c4ae84077',1,'FontRenderabilityCache']]],
+  ['wxmaximaicon_13',['wxMaximaIcon',['../wxMaximaIcon_8cpp.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp'],['../wxMaximaIcon_8h.html#aab835d20b3d7db04a8c1198dcbef93c9',1,'wxMaximaIcon():&#160;wxMaximaIcon.cpp']]],
+  ['wxmaximaoperatingsystemdescription_14',['wxMaximaOperatingSystemDescription',['../wxMaximaOSDescription_8cpp.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp'],['../wxMaximaOSDescription_8h.html#a9456b72b11c75f952bb7d4a99d5a9232',1,'wxMaximaOperatingSystemDescription():&#160;wxMaximaOSDescription.cpp']]]
 ];
