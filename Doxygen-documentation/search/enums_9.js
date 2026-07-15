77a9018fe686cb43f166dec5a6e58852cf3b6d2b
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-15 09:23:26 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@021484cfd048aa656b00b12c45cb98dae45b1059 🚀

--- Doxygen-documentation/search/enums_9.js
+++ Doxygen-documentation/search/enums_9.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['matchpart_0',['MatchPart',['../WorksheetSearch_8h.html#a7cda527bad1a211ecffff93ec930b18f',1,'WorksheetSearch']]]
+  ['matchpart_0',['MatchPart',['../WorksheetSearch_8h.html#a7cda527bad1a211ecffff93ec930b18f',1,'WorksheetSearch']]],
+  ['maximastatus_1',['MaximaStatus',['../classStatusBar.html#af1235c09bed081ebb46dbd8b264f5c96',1,'StatusBar']]]
 ];
--- Doxygen-documentation/search/enumvalues_2.js
+++ Doxygen-documentation/search/enumvalues_2.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['demofile_0',['demofile',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29a653d70d312cae37be23d27c9526317f7',1,'AutoComplete']]],
-  ['disconnected_1',['DISCONNECTED',['../classMaxima.html#a01e208858ffeeb60e8a1787a580103c8ab062169d756676320e790966e56ad5ce',1,'Maxima']]]
+  ['debugging_0',['debugging',['../classStatusBar.html#af1235c09bed081ebb46dbd8b264f5c96a4420d8cdc87e59da070dc288d199b1b3',1,'StatusBar']]],
+  ['demofile_1',['demofile',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29a653d70d312cae37be23d27c9526317f7',1,'AutoComplete']]],
+  ['disconnected_2',['DISCONNECTED',['../classMaxima.html#a01e208858ffeeb60e8a1787a580103c8ab062169d756676320e790966e56ad5ce',1,'Maxima']]]
 ];
