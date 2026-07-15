094e104b14f2f0bd6490e2d128612c18a9742c67
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-15 12:14:20 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@ff5f6dc91f27f5416b89c581205f6e5a5db1b4b5 🚀

--- Doxygen-documentation/search/enumvalues_8.js
+++ Doxygen-documentation/search/enumvalues_8.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['loadfile_0',['loadfile',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29ae79474d174763269e5ba387002436e43',1,'AutoComplete']]],
-  ['low_1',['Low',['../classBackgroundTask.html#a217da7631d4231f559cbad8f0e7e1566a28d0edd045e05cf5af64e35ae0c4c6ef',1,'BackgroundTask']]]
+  ['lispmode_0',['lispmode',['../classStatusBar.html#af1235c09bed081ebb46dbd8b264f5c96a6622d76440ce861af9b538f762e7fe68',1,'StatusBar']]],
+  ['loadfile_1',['loadfile',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29ae79474d174763269e5ba387002436e43',1,'AutoComplete']]],
+  ['low_2',['Low',['../classBackgroundTask.html#a217da7631d4231f559cbad8f0e7e1566a28d0edd045e05cf5af64e35ae0c4c6ef',1,'BackgroundTask']]]
 ];
