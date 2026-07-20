d6a1c9b2116f85004f6e94f6ef18c9015fa6bb55
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-07-20 17:10:51 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@3a7deba1558cfa9b2774faf0955988794100ecc0 🚀

--- Doxygen-documentation/search/enumvalues_1.js
+++ Doxygen-documentation/search/enumvalues_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['checkedifequal_0',['CheckedIfEqual',['../structMaximaMenuSyncRow.html#aab1861fb80f5b88b3b3c6f6abba42efaa2e292948025a018a6f5fd7d7fc7f497d',1,'MaximaMenuSyncRow']]]
+  ['cellgone_0',['CellGone',['../classWorksheetDocument.html#a597da4ddc26d35569b9346897dff3b72a2df905ed9fb0e7f499818b94f3dbe3c4',1,'WorksheetDocument']]],
+  ['checkedifequal_1',['CheckedIfEqual',['../structMaximaMenuSyncRow.html#aab1861fb80f5b88b3b3c6f6abba42efaa2e292948025a018a6f5fd7d7fc7f497d',1,'MaximaMenuSyncRow']]]
 ];
--- Doxygen-documentation/search/enumvalues_a.js
+++ Doxygen-documentation/search/enumvalues_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['numberofstyles_0',['NUMBEROFSTYLES',['../TextStyle_8h.html#a68ac52da6129614b3fa14e7c828c1993a4ff5d935a79c3cd0c1e5d3a188645046',1,'TextStyle.h']]],
-  ['numberoftypes_1',['numberOfTypes',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29a83b932a902206b352f3795faf6f2d231',1,'AutoComplete']]]
+  ['noopskipped_0',['NoOpSkipped',['../classWorksheetDocument.html#a597da4ddc26d35569b9346897dff3b72a773b1ab79b5aa284fc01d1ebb6e73c52',1,'WorksheetDocument']]],
+  ['numberofstyles_1',['NUMBEROFSTYLES',['../TextStyle_8h.html#a68ac52da6129614b3fa14e7c828c1993a4ff5d935a79c3cd0c1e5d3a188645046',1,'TextStyle.h']]],
+  ['numberoftypes_2',['numberOfTypes',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29a83b932a902206b352f3795faf6f2d231',1,'AutoComplete']]]
 ];
