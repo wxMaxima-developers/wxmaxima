b1a8cfaf396ead4d32ac8ed59b830748dfd5a928
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-08-17 16:59:48 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@540b86809bcf2657fed4ea9cd79739a59dc1d272 🚀

--- Doxygen-documentation/search/enumvalues_3.js
+++ Doxygen-documentation/search/enumvalues_3.js
@@ -2,5 +2,11 @@ var searchData=
 [
   ['esccommand_0',['esccommand',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29aee4022f4d28eb504969b3042359544e4',1,'AutoComplete']]],
   ['eval1_1',['EVal1',['../classAfterdoc__Test.html#adab0cd7ad3b4875e245ca8f6238a388aae054276790e35692ad0abe10c5b75da4',1,'Afterdoc_Test']]],
-  ['eval2_2',['EVal2',['../classAfterdoc__Test.html#adab0cd7ad3b4875e245ca8f6238a388aac849f37624d8d2d68ca72c4a8df9cf99',1,'Afterdoc_Test']]]
+  ['eval2_2',['EVal2',['../classAfterdoc__Test.html#adab0cd7ad3b4875e245ca8f6238a388aac849f37624d8d2d68ca72c4a8df9cf99',1,'Afterdoc_Test']]],
+  ['exit_5ffile_5fopen_5ffailed_3',['EXIT_FILE_OPEN_FAILED',['../classwxMaxima.html#a95a985202220a6392975ffe21c92cafaa1ff5f74ae87f27b2f3239dd4a88d2300',1,'wxMaxima']]],
+  ['exit_5ffile_5fsave_5ffailed_4',['EXIT_FILE_SAVE_FAILED',['../classwxMaxima.html#a95a985202220a6392975ffe21c92cafaa815e9ea7ef14a1fa1fe87fd57e29551c',1,'wxMaxima']]],
+  ['exit_5fimage_5fdecode_5ffailed_5',['EXIT_IMAGE_DECODE_FAILED',['../classwxMaxima.html#a95a985202220a6392975ffe21c92cafaafb378826b0d3e1dbbd6955adea320117',1,'wxMaxima']]],
+  ['exit_5fimage_5fload_5ffailed_6',['EXIT_IMAGE_LOAD_FAILED',['../classwxMaxima.html#a95a985202220a6392975ffe21c92cafaaef18e7887e0f97881fe8b718ff2dd429',1,'wxMaxima']]],
+  ['exit_5fmaxima_5ferror_7',['EXIT_MAXIMA_ERROR',['../classwxMaxima.html#a95a985202220a6392975ffe21c92cafaa4d7daa8c47689231a4e7d651c7558e37',1,'wxMaxima']]],
+  ['exit_5funanswered_5fquestion_8',['EXIT_UNANSWERED_QUESTION',['../classwxMaxima.html#a95a985202220a6392975ffe21c92cafaa79ef44494781ef42edac4a6309bf3071',1,'wxMaxima']]]
 ];
