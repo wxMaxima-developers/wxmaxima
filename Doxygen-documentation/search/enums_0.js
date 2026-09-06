7be86a712c63525905d2cd6152cf6f3b50e21317
gunterkoenigsmann <gunterkoenigsmann@users.noreply.github.com>
2026-09-06 16:51:04 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@f67c08adc4b42ea2ea4d69a5fda4e9e459309c70 🚀

--- Doxygen-documentation/search/enums_0.js
+++ Doxygen-documentation/search/enums_0.js
@@ -1,8 +1,9 @@
 var searchData=
 [
-  ['animationstartstopstate_0',['AnimationStartStopState',['../classToolBar.html#a327b655c366e7932f751d2815cc629fc',1,'ToolBar']]],
-  ['anotherenum_1',['AnotherEnum',['../classEnum__Test.html#a633286511e19b996e97699d7dd2cd2a0',1,'Enum_Test']]],
-  ['appearance_2',['Appearance',['../classConfiguration.html#a0c3c003a32828fe343fb6e3b616a4657',1,'Configuration']]],
-  ['appendopt_3',['AppendOpt',['../classMaximaOutputAppender.html#ab1a99b2b284ce8585493a4ec95e2d2e4',1,'MaximaOutputAppender']]],
-  ['autocompletiontype_4',['autoCompletionType',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29',1,'AutoComplete']]]
+  ['aiproviderkind_0',['AiProviderKind',['../AiProvider_8h.html#afa76ab3b3e4c4cf3e53d5fb066042f04',1,'AiProvider.h']]],
+  ['animationstartstopstate_1',['AnimationStartStopState',['../classToolBar.html#a327b655c366e7932f751d2815cc629fc',1,'ToolBar']]],
+  ['anotherenum_2',['AnotherEnum',['../classEnum__Test.html#a633286511e19b996e97699d7dd2cd2a0',1,'Enum_Test']]],
+  ['appearance_3',['Appearance',['../classConfiguration.html#a0c3c003a32828fe343fb6e3b616a4657',1,'Configuration']]],
+  ['appendopt_4',['AppendOpt',['../classMaximaOutputAppender.html#ab1a99b2b284ce8585493a4ec95e2d2e4',1,'MaximaOutputAppender']]],
+  ['autocompletiontype_5',['autoCompletionType',['../classAutoComplete.html#a6225a91b46c0459755a62de137620b29',1,'AutoComplete']]]
 ];
--- Doxygen-documentation/search/variables_f.js
+++ Doxygen-documentation/search/variables_f.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['righttoleft_0',['rightToLeft',['../structBidiRun.html#a3a06760adcb00e1780dd161a09a8371b',1,'BidiRun']]]
+  ['righttoleft_0',['rightToLeft',['../structBidiRun.html#a3a06760adcb00e1780dd161a09a8371b',1,'BidiRun']]],
+  ['role_1',['role',['../structAiChatMessage.html#a7108d7feea6e305192a39177192af2ac',1,'AiChatMessage']]]
 ];
