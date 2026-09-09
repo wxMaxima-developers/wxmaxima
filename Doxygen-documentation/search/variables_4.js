e2743bc4b21af2f66ad2d5a4c4c689c019e4d396
daute <github-mail@wolfgang.dautermann.at>
2026-09-09 20:55:32 +0000

    Deploying to gh-pages from @ wxMaxima-developers/wxmaxima@951b811e929f933e5a8d770ae0355a3aeda67cef 🚀

--- Doxygen-documentation/search/variables_4.js
+++ Doxygen-documentation/search/variables_4.js
@@ -4,6 +4,5 @@ var searchData=
   ['description_1',['description',['../structCharButton_1_1Definition.html#ab873b11925f89f2e13c50e507c4e4319',1,'CharButton::Definition']]],
   ['diffsync_5fno_5fcell_2',['DIFFSYNC_NO_CELL',['../DiffScrollSync_8h.html#aadc5553c667fa03eb20753894f654105',1,'DiffScrollSync.h']]],
   ['din_5f0_3',['din_0',['../classmux__using__with.html#a9fc14b65611c7124d4cab27b963f3011',1,'mux_using_with']]],
-  ['din_5f1_4',['din_1',['../classmux__using__with.html#aaedccad88ca9b207c86a561342fcbaa1',1,'mux_using_with']]],
-  ['displayname_5',['displayName',['../structConfigDialogue_1_1AiProviderUiRecord.html#ab4e361bf255ee5f0f19a6807d27791c5',1,'ConfigDialogue::AiProviderUiRecord']]]
+  ['din_5f1_4',['din_1',['../classmux__using__with.html#aaedccad88ca9b207c86a561342fcbaa1',1,'mux_using_with']]]
 ];
